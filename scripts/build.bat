761618baebe0eca3d14e2c5f7f174cdc80cded41
blagoev <lubo@blagoev.com>
2021-09-14 13:41:32 +0300

    add short build doc

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,3 +1,7 @@
+@REM This scripts assumes in-source building where the project directory is one dir up. 
+@REM Output is in PROJECT_DIR\binary directory 
+@REM example usage: ....\realm-dart\build>..\scripts\build.bat
+
 cmake ^
     -G "Visual Studio 16 2019" ^
     -T host=x86 ^
