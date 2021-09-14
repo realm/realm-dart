512b8e73568d26578df29a27c838dc7b8fafe15d
blagoev <lubo@blagoev.com>
2021-09-14 11:51:38 +0300

    add src/CMakeLists.txt - builds and outputs the binaries to the <src_root>/binary directory
    add a dummy mention to realm_dart native binary to force windows linker to actually export all realm-core C API exports

---
+++ src/realm_dart.cpp
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2021 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <exception>
+
+#include <realm.h>
+
+#if defined(_WIN32)
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+BOOL APIENTRY DllMain(HMODULE module,
+                      DWORD  reason,
+                      LPVOID reserved) {
+  return true;
+}
+
+// Win only. Force the linker to link all exports from realm-core C API
+const char* dummy(void) {
+	return realm_get_library_version();
+}
+
+#endif  // defined(_WIN32)
