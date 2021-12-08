059ab31b434f8af90551997f15212a60f2311ebd
blagoev <lubo@blagoev.com>
2021-12-08 22:37:26 +0200

    fix main

--- lib/main.gen.dart
+++ lib/main.gen.dart
@@ -10,7 +10,7 @@ part of 'main.dart';
 class Car extends _Car with RealmObject {
   
   @override
-  String get make => RealmObject.get<String>(this, "make");
+  String get make => RealmObject.get<String>(this, "make") as String;
   @override
   set make(String value) => RealmObject.set<String>(this, "make", value);
 
@@ -24,7 +24,7 @@ class Person extends _Person with RealmObject {
   // ignore_for_file: unused_element, unused_local_variable
 
   @override
-  String get name => RealmObject.get<String>(this, "name");
+  String get name => RealmObject.get<String>(this, "name") as String;
   @override
   set name(String value) => RealmObject.set<String>(this, "name", value);
 
