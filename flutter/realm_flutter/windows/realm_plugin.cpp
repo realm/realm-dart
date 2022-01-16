dc9228403a404a7d652498829e63cb9c56bb0724
blagoev <lubo@blagoev.com>
2022-01-16 16:46:26 +0200

    add initial Flutter for Windows support
    fix flutter example app to compile and run with the alpha version
    fix init realm on Flutter for Windows

--- flutter/realm_flutter/windows/realm_plugin.cpp
+++ flutter/realm_flutter/windows/realm_plugin.cpp
@@ -3,9 +3,6 @@
 // This must be included before many other Windows headers.
 #include <windows.h>
 
-// For getPlatformVersion; remove unless needed for your plugin implementation.
-#include <VersionHelpers.h>
-
 #include <flutter/method_channel.h>
 #include <flutter/plugin_registrar_windows.h>
 #include <flutter/standard_method_codec.h>
@@ -14,69 +11,56 @@
 #include <memory>
 #include <sstream>
 
-namespace {
+namespace
+{
+  class RealmPlugin : public flutter::Plugin
+  {
+  public:
+    static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
 
-class RealmPlugin : public flutter::Plugin {
- public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
+    RealmPlugin();
 
-  RealmPlugin();
+    virtual ~RealmPlugin();
 
-  virtual ~RealmPlugin();
+  private:
+    // Called when a method is called on this plugin's channel from Dart.
+    void HandleMethodCall(
+        const flutter::MethodCall<flutter::EncodableValue> &method_call,
+        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+  };
 
- private:
-  // Called when a method is called on this plugin's channel from Dart.
-  void HandleMethodCall(
+  // static
+  void RealmPlugin::RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar)
+  {
+    //TODO: these channels seem unneccessary. Remove if not needed
+    auto channel = std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(registrar->messenger(), "realm", &flutter::StandardMethodCodec::GetInstance());
+
+    auto plugin = std::make_unique<RealmPlugin>();
+
+    channel->SetMethodCallHandler(
+        [plugin_pointer = plugin.get()](const auto &call, auto result)
+        {
+          plugin_pointer->HandleMethodCall(call, std::move(result));
+        });
+
+    registrar->AddPlugin(std::move(plugin));
+  }
+
+  RealmPlugin::RealmPlugin() {}
+
+  RealmPlugin::~RealmPlugin() {}
+
+  void RealmPlugin::HandleMethodCall(
       const flutter::MethodCall<flutter::EncodableValue> &method_call,
-      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-};
-
-// static
-void RealmPlugin::RegisterWithRegistrar(
-    flutter::PluginRegistrarWindows *registrar) {
-  auto channel =
-      std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
-          registrar->messenger(), "realm",
-          &flutter::StandardMethodCodec::GetInstance());
-
-  auto plugin = std::make_unique<RealmPlugin>();
-
-  channel->SetMethodCallHandler(
-      [plugin_pointer = plugin.get()](const auto &call, auto result) {
-        plugin_pointer->HandleMethodCall(call, std::move(result));
-      });
-
-  registrar->AddPlugin(std::move(plugin));
-}
-
-RealmPlugin::RealmPlugin() {}
-
-RealmPlugin::~RealmPlugin() {}
-
-void RealmPlugin::HandleMethodCall(
-    const flutter::MethodCall<flutter::EncodableValue> &method_call,
-    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  if (method_call.method_name().compare("getPlatformVersion") == 0) {
-    std::ostringstream version_stream;
-    version_stream << "Windows ";
-    if (IsWindows10OrGreater()) {
-      version_stream << "10+";
-    } else if (IsWindows8OrGreater()) {
-      version_stream << "8";
-    } else if (IsWindows7OrGreater()) {
-      version_stream << "7";
-    }
-    result->Success(flutter::EncodableValue(version_stream.str()));
-  } else {
+      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
+  {
     result->NotImplemented();
   }
-}
 
-}  // namespace
+} // namespace
 
 void RealmPluginRegisterWithRegistrar(
-    FlutterDesktopPluginRegistrarRef registrar) {
-  RealmPlugin::RegisterWithRegistrar(
-      flutter::PluginRegistrarManager::GetInstance()
-          ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
+    FlutterDesktopPluginRegistrarRef registrar)
+{
+  RealmPlugin::RegisterWithRegistrar(flutter::PluginRegistrarManager::GetInstance()->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
 }
