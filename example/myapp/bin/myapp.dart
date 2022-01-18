7668d0906cd565e9e779b31b9224e106f077bdd3
blagoev <lubo@blagoev.com>
2022-01-18 23:17:06 +0200

    update example

--- example/myapp/bin/myapp.dart
+++ example/myapp/bin/myapp.dart
@@ -1,36 +1,29 @@
 import 'package:realm_dart/realm.dart';
 
 part 'myapp.g.dart';
-/*
-class _Car {
-  @RealmProperty()
-  late String make;
-}
 
-class _Person {
-  @RealmProperty()
-  late String name; 
+@RealmModel()
+class _Car {
+  @PrimaryKey()
+  late final String make;
 }
 
 void main(List<String> arguments) {
-  var config = Configuration();
-  config.schema.add(Car);
-
-  var realm = new Realm(config);
+  var config = Configuration([Car.schema]);
+  var realm = Realm(config);
 
   realm.write(() {
     print('Creating Realm object of type Car');
-    var car = realm.create(new Car()..make = "Audi");
+    var car = realm.add(Car("Audi"));
     print('The car is ${car.make}');
     
     car.make = "VW";
     print("The car is ${car.make}");
   });
 
-  var objects = realm.objects<Car>();
+  var objects = realm.all<Car>();
   var indexedCar = objects[0];
   print('The indexedCar is ${indexedCar.make}');
 
   print("Done");
 }
-*/
