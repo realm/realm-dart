66f0e644495956a21e550a0bddb9f2cc3f6f4179
Desislava Stefanova <dst.stefanova@gmail.com>
2022-02-25 13:37:40 +0200

    Snapshot test added

--- test/results_test.dart
+++ test/results_test.dart
@@ -138,7 +138,7 @@ Future<void> main([List<String>? args]) async {
     realm.close();
   });
 
-  test('Results iteration test', () {
+  test('Results iterating test', () {
     var config = Configuration([Team.schema, Person.schema]);
     var realm = Realm(config);
 
@@ -160,6 +160,45 @@ Future<void> main([List<String>? args]) async {
 
     //Ensure list size is the same like teams collection size
     expect(list.length, teams.length);
+    realm.close();
+  });
+
+  test('Results snapshot iterating test', () {
+    var config = Configuration([Team.schema, Person.schema]);
+    var realm = Realm(config);
+
+    //Create two teams
+    realm.write(() {
+      realm.add(Team("team One"));
+      realm.add(Team("team Two"));
+    });
+
+    //Reload teams from realm and ensure they exist
+    var teams = realm.all<Team>();
+    expect(teams.length, 2);
+
+    //Adding new teams to real while iterating through them.
+    //Iterator use a snapshot of results collection and ignores newly added teams.
+    List<Team> list = [];
+    for (Team team in teams) {
+      list.add(team);
+      realm.write(() {
+        realm.add(Team("new team"));
+      });
+    }
+    //Ensure list size is the same like teams collection size was at the beginnig
+    expect(list.length, 2);
+
+    //Ensure teams collection is increased
+    expect(teams.length, 4);
+
+    //Iterating teams again will create a snapshot with the newly added items
+    list.clear();
+    for (Team team in teams) {
+      list.add(team);
+    }
+    expect(list.length, teams.length);
+
     realm.close();
   });
 
@@ -211,7 +250,6 @@ Future<void> main([List<String>? args]) async {
 
     realm.close();
   });
-
 
   test('Query results with no arguments throws', () {
     var config = Configuration([Car.schema]);
