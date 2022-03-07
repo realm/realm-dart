3a226e85b9273fa0b825df2bb2c01830e122ba6a
Kasper Overgård Nielsen <kasper.nielsen@mongodb.com>
2022-03-07 13:43:15 +0100

    Drop drop default platform channel method: getPlatformVersion

--- flutter/realm_flutter/linux/realm_plugin.cpp
+++ flutter/realm_flutter/linux/realm_plugin.cpp
@@ -22,17 +22,7 @@ static void realm_plugin_handle_method_call(
     FlMethodCall* method_call) {
   g_autoptr(FlMethodResponse) response = nullptr;
 
-  const gchar* method = fl_method_call_get_name(method_call);
-
-  if (strcmp(method, "getPlatformVersion") == 0) {
-    struct utsname uname_data = {};
-    uname(&uname_data);
-    g_autofree gchar *version = g_strdup_printf("Linux %s", uname_data.version);
-    g_autoptr(FlValue) result = fl_value_new_string(version);
-    response = FL_METHOD_RESPONSE(fl_method_success_response_new(result));
-  } else {
-    response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
-  }
+  response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
 
   fl_method_call_respond(method_call, response, nullptr);
 }
