20dd56d154d889391bc8405a65c1de0b726ec179
Desislava Stefanova <dst.stefanova@gmail.com>
2022-03-18 00:50:15 +0200

    Test refactoring by folders. Running tests for all existing inputs.

--- generator/test/good_test.dart
+++ generator/test/good_test.dart
@@ -1,20 +1,10 @@
-
 import 'test_util.dart';
 
 void main() {
-  final tests = {
-    'required argument with default value',
-    'required argument',
-    'list initialization',
-    'optional argument',
-    'user defined getter',
-  };
-
-  for (var testData in tests) {
-    var fileName = testData.replaceAll(' ', '_');
-
-    generatorTest(testData, () async {
-      await generatorTestBuilder('good_test_io', '$fileName.dart', '$fileName.expected');
+  const directory = 'test/good_test_data';
+  getListOfTestFiles(directory).forEach((inputFile, outputFile) {
+    executeTest(getTestName(inputFile), () async {
+      await generatorTestBuilder(directory, inputFile, outputFile);
     });
-  }
+  });
 }
