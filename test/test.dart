1adabf51017eb8d2ae9428ff84890f5dfd130436
Nikola Irinchev <irinchev@me.com>
2022-03-31 18:16:02 +0200

    Use a helper to create a realm instance

--- test/test.dart
+++ test/test.dart
@@ -16,6 +16,7 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
+import 'dart:collection';
 import 'dart:io';
 import 'dart:math';
 import 'package:path/path.dart' as _path;
@@ -76,6 +77,8 @@ class _School {
 String? testName;
 Map<String, BaasApp> baasApps = <String, BaasApp>{};
 
+final _openRealms = Queue<Realm>();
+
 //Overrides test method so we can filter tests
 void test(String? name, dynamic Function() testFunction, {dynamic skip}) {
   if (testName != null && !name!.contains(testName!)) {
@@ -108,26 +111,37 @@ Future<void> setupTests(List<String>? args) async {
     Configuration.defaultPath = path;
 
     addTearDown(() async {
-      var file = File(path);
-      try {
-        Realm.deleteRealm(path);
-      } catch (e) {
-        fail("Can not delete realm at path: $path. Did you forget to close it?");
+      final paths = HashSet<String>();
+      paths.add(path);
+
+      while (_openRealms.isNotEmpty) {
+        final realm = _openRealms.removeFirst();
+        paths.add(realm.config.path);
+        realm.close();
       }
 
-      if (await file.exists() && file.path.endsWith(".realm")) {
-        await tryDeleteFile(file);
-      }
+      for (final path in paths) {
+        var file = File(path);
+        try {
+          Realm.deleteRealm(path);
+        } catch (e) {
+          fail("Can not delete realm at path: $path. Did you forget to close it?");
+        }
 
-      file = File("$path.lock");
-      if (await file.exists()) {
-        await tryDeleteFile(file);
-      }
+        if (await file.exists() && file.path.endsWith(".realm")) {
+          await tryDeleteFile(file);
+        }
 
-      final dir = Directory("$path.management");
-      if (await dir.exists()) {
-        if ((await dir.stat()).type == FileSystemEntityType.directory) {
-          await tryDeleteFile(dir, recursive: true);
+        file = File("$path.lock");
+        if (await file.exists()) {
+          await tryDeleteFile(file);
+        }
+
+        final dir = Directory("$path.management");
+        if (await dir.exists()) {
+          if ((await dir.stat()).type == FileSystemEntityType.directory) {
+            await tryDeleteFile(dir, recursive: true);
+          }
         }
       }
     });
@@ -140,6 +154,12 @@ final random = Random();
 String generateRandomString(int len) {
   const _chars = 'abcdefghjklmnopqrstuvwxuz';
   return List.generate(len, (index) => _chars[random.nextInt(_chars.length)]).join();
+}
+
+Realm getRealm(Configuration config) {
+  final realm = Realm(config);
+  _openRealms.add(realm);
+  return realm;
 }
 
 Future<void> tryDeleteFile(FileSystemEntity fileEntity, {bool recursive = false}) async {
