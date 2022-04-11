0baa62f76d0178e30779dea858791a5511ac2952
Kasper Overgård Nielsen <kasper@byolimit.com>
2022-04-11 09:21:38 +0200

    Address PR feedback

--- lib/src/application_configuration.dart
+++ lib/src/application_configuration.dart
@@ -23,9 +23,11 @@ import 'package:pub_semver/pub_semver.dart';
 /// Specify if and how to persists user objects.
 enum MetadataPersistenceMode {
   /// Persist [User] objects, but do not encrypt them.
-  unencrypted, 
+  unencrypted,
+
   /// Persist [User] objects in an encrypted store.
   encrypted,
+
   /// Do not persist [User] objects.
   disabled,
 }
@@ -36,45 +38,45 @@ class ApplicationConfiguration {
   final String appId;
 
   /// The [baseFilePath] is the [Directory] relative to which all local data for this application will be stored.
-  /// 
-  /// This data includes metadata for users and synchronized Realms. If set, you must ensure that the [baseFilePath] 
+  ///
+  /// This data includes metadata for users and synchronized Realms. If set, you must ensure that the [baseFilePath]
   /// directory exists.
-  final Directory? baseFilePath;
+  final Directory baseFilePath;
 
   /// The [baseUrl] is the [Uri] used to reach the MongoDB Realm server.
-  /// 
+  ///
   /// [baseUrl] only needs to be set if for some reason your application isn't hosted on realm.mongodb.com.
   /// This can be the case if you're testing locally or are using a pre-production environment.
-  final Uri? baseUrl;
+  final Uri baseUrl;
 
   /// The [defaultRequestTimeout] for HTTP requests performed as part of authentication.
-  final Duration? defaultRequestTimeout;
+  final Duration defaultRequestTimeout;
 
   /// The [localAppName] is the friendly name identifying the current client application.
-  /// 
+  ///
   /// This is typically used to differentiate between client applications that use the same
-  /// MongoDB Realm app. 
-  /// 
+  /// MongoDB Realm app.
+  ///
   /// These can be the same conceptual app developed for different platforms, or
   /// significantly different client side applications that operate on the same data - e.g. an event managing
   /// service that has different clients apps for organizers and attendees.
   final String? localAppName;
 
-  /// The [localAppVersion] can be specified, if you wish to distinguish different client versions of the 
+  /// The [localAppVersion] can be specified, if you wish to distinguish different client versions of the
   /// same application.
   final Version? localAppVersion;
 
   final MetadataPersistenceMode metadataPersistenceMode;
 
-  /// The encryption key to use for user metadata on this device, if [metadataPersistenceMode] is 
-  /// [MetadataPersistenceMode.encrypted]. 
-  /// 
+  /// The encryption key to use for user metadata on this device, if [metadataPersistenceMode] is
+  /// [MetadataPersistenceMode.encrypted].
+  ///
   /// The [metadataEncryptionKey] must be exactly 64 bytes.
   /// Setting this will not change the encryption key for individual Realms, which is set in the [Configuration].
   final List<int>? metadataEncryptionKey;
 
   /// The [HttpClient] that will be used for HTTP requests during authentication.
-  /// 
+  ///
   /// You can use this to override the default http client handler and configure settings like proxies,
   /// client certificates, and cookies. While these are not required to connect to MongoDB Realm under
   /// normal circumstances, they can be useful if client devices are behind corporate firewall or use
@@ -84,13 +86,15 @@ class ApplicationConfiguration {
   /// Instantiates a new [ApplicationConfiguration].
   ApplicationConfiguration(
     this.appId, {
-    this.baseUrl,
-    this.baseFilePath,
-    this.defaultRequestTimeout,
+    Uri? baseUrl,
+    Directory? baseFilePath,
+    this.defaultRequestTimeout = const Duration(milliseconds: 60000),
     this.localAppName,
     this.localAppVersion,
     this.metadataPersistenceMode = MetadataPersistenceMode.unencrypted,
     this.metadataEncryptionKey,
     HttpClient? httpClient,
-  }) : httpClient = httpClient ?? HttpClient();
+  })  : baseUrl = baseUrl ?? Uri.parse('https://realm.mongodb.com'),
+        baseFilePath = baseFilePath ?? Directory.current,
+        httpClient = httpClient ?? HttpClient();
 }
