fc63d675a56e42f23b4fd8e428ac076e913dce26
blagoev <lubo@blagoev.com>
2022-04-22 13:18:16 +0300

    fix syntax in credentials.dart

--- lib/src/credentials.dart
+++ lib/src/credentials.dart
@@ -102,7 +102,8 @@ class EmailPasswordAuthProvider {
   /// Calls the reset password function, configured on the server.
   Future<void> callResetPasswordFunction(String email, String password, Map<String, dynamic> functionArgs) {
     return realmCore.emailPasswordCallResetPasswordFunction(application, email, password, jsonEncode(functionArgs));
-
+  }
+  
   /// Retries the custom confirmation function on a user for a given email.
   Future<void> retryCustomConfirmationFunction(String email) {
     return realmCore.emailPasswordRetryCustomConfirmationFunction(application, email);
