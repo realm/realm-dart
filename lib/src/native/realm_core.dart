3fe824e848b7c2a35b4b2c44117acb78c85d08e6
blagoev <lubo@blagoev.com>
2022-04-23 09:06:25 +0300

    support remove user

--- lib/src/native/realm_core.dart
+++ lib/src/native/realm_core.dart
@@ -1078,6 +1078,20 @@ class _RealmCore {
       return userHandles;
     });
   }
+
+  Future<void> removeUser(App app, User user) async {
+    final completer = Completer<void>();
+    _realmLib.invokeGetBool(
+        () => _realmLib.realm_app_remove_user(
+              app.handle._pointer,
+              user.handle._pointer,
+              Pointer.fromFunction(void_completion_callback),
+              completer.toPersistentHandle(),
+              _deletePersistentHandleFuncPtr,
+            ),
+        "Remove user failed");
+    return completer.future;
+  }
 }
 
 class LastError {
