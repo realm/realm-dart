1fe8d08d333112648dc552386fd5cacee53eeb76
Yavor Georgiev <fealebenpae@gmail.com>
2022-04-27 16:37:31 +0200

    make things explicit

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -6,5 +6,5 @@ set -o pipefail
 # Start in the root directory of the project.
 cd "$(dirname "$0")/.."
 
-cmake --preset default
-cmake --build --preset default --config MinSizeRel
+cmake --preset linux
+cmake --build --preset linux --config MinSizeRel
