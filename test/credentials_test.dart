f4643c30a7e52fa9995984cd4a5ab8e564df8228
Desislava Stefanova <dst.stefanova@gmail.com>
2022-04-27 13:53:19 +0300

    Manual test for user confirmation

--- test/credentials_test.dart
+++ test/credentials_test.dart
@@ -74,7 +74,6 @@ Future<void> main([List<String>? args]) async {
     await authProvider.registerUser(username, password);
   }, appName: "auto");
 
-
   baasTest('Email/Password - register user twice throws', (configuration) async {
     final app = App(configuration);
     final authProvider = EmailPasswordAuthProvider(app);
@@ -129,4 +128,35 @@ Future<void> main([List<String>? args]) async {
       await authProvider.confirmUser("abc", "123");
     }, throws<RealmException>("invalid token data"));
   }, appName: "email");
+
+  group("Email/Password - confirm user - manual tests", () {
+    // The tests in this group are for manual testing, since they require interaction with mail box.
+    // Please enter a valid data in the variables under comments.
+    // Run test 1, then copy token and tokenId from mail box.
+    // Set the variables with token details and then run test 2.
+    // Go to the application and check whether the new registered user is confirmed.
+    // Make sure the email haven't been already registered in apllication.
+
+    // Enter a valid email that is not registered
+    String username = "existing@mail.com";
+    String password = "SWV23R#@T#VFQDV";
+
+    baasTest('1. Register a valid user for email confirmation', (configuration) async {
+      final app = App(configuration);
+      final authProvider = EmailPasswordAuthProvider(app);
+      await authProvider.registerUser(username, password);
+    }, appName: "email", skip: "It is a manual test");
+
+    baasTest('2. Take the recieved token from the email and confirm the user', (configuration) async {
+      //Enter valid token and tokenId from email
+      String token = "092ce189b8428242239704d9cbe6678ba967649e9d5aec03e4bc01d5e195f3d9ee020104dfa4a0a74a3565a782948b52f48fea64f7382b5e39303c062a6175a6";
+      String tokenId = "62691daa3a22b87daa20237e";
+
+      final app = App(configuration);
+      final authProvider = EmailPasswordAuthProvider(app);
+      await authProvider.confirmUser(token, tokenId);
+      final user = await app.logIn(Credentials.emailPassword(username, password));
+      await app.logout(user);
+    }, appName: "email", skip: "Run this test manually after test 1 and after setting token and tokenId");
+  });
 }
