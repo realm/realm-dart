50d1edd9bcb43de8e753d2813fb9fd01c153bba1
Kasper Overgård Nielsen <kasper@byolimit.com>
2022-05-02 10:41:06 +0200

    Add version & state properties to SubscriptionSet
    Also, SubscriptionSet.remove* are now void methods, and corresponding
    _RealmCore functions uses invokeGetBool.

--- test/subscription_test.dart
+++ test/subscription_test.dart
@@ -16,58 +16,99 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
+import 'dart:async';
 import 'dart:io';
 
+import 'package:meta/meta.dart';
 import 'package:test/expect.dart';
 
 import '../lib/realm.dart';
 import '../lib/src/configuration.dart';
 import 'test.dart';
 
-Future<void> main([List<String>? args]) async {
-  print("Current PID $pid");
-
-  await setupTests(args);
-
-  baasTest('Get subscriptions', (appConfiguration) async {
+@isTest
+void testSubscriptions(String name, FutureOr<void> Function(Realm) tester) async {
+  baasTest(name, (appConfiguration) async {
     final app = App(appConfiguration);
     final credentials = Credentials.anonymous();
     final user = await app.logIn(credentials);
     final configuration = (Configuration.flexibleSync(user, [Task.schema]) as FlexibleSyncConfiguration)..sessionStopPolicy = SessionStopPolicy.immediately;
     final realm = getRealm(configuration);
+    try {
+      await tester(realm);
+    } finally {
+      realm.close();
+    }
+  });
+}
 
-    expect(realm.subscriptions, isEmpty);
+Future<void> main([List<String>? args]) async {
+  print("Current PID $pid");
+
+  await setupTests(args);
+
+  test('Get subscriptions throws on wrong configuration', () {
+    final config = Configuration.local([Task.schema]);
+    final realm = getRealm(config);
+    expect(() => realm.subscriptions, throws<RealmError>());
+  });
+
+  testSubscriptions('SubscriptionSet.state', (realm) {
+    expect(realm.subscriptions.state, SubscriptionSetState.uncommitted);
+  });
+
+  testSubscriptions('SubscriptionSet.version', (realm) async {
+    final subscriptions = realm.subscriptions;
+    expect(subscriptions.version, 0);
+
+    subscriptions.update((mutableSubscriptions) {
+      mutableSubscriptions.addOrUpdate(realm.all<Task>());
+    });
+
+    expect(subscriptions.length, 1);
+    expect(subscriptions.version, 1);
+
+    subscriptions.update((mutableSubscriptions) {
+      mutableSubscriptions.removeAll();
+    });
+
+    expect(subscriptions.length, 0);
+    expect(subscriptions.version, 2);
+  });
+
+  testSubscriptions('Get subscriptions', (realm) async {
+    final subscriptions = realm.subscriptions;
+
+    expect(subscriptions, isEmpty);
 
     final query = realm.all<Task>();
 
-    realm.subscriptions!.update((mutableSet) {
-      mutableSet.addOrUpdate(query);
+    subscriptions.update((mutableSubscriptions) {
+      mutableSubscriptions.addOrUpdate(query);
     });
 
-    expect(realm.subscriptions!.length, 1);
+    expect(subscriptions.length, 1);
 
-    realm.subscriptions!.update((mutableSubscriptions) {
+    subscriptions.update((mutableSubscriptions) {
       mutableSubscriptions.remove(query);
     });
 
-    expect(realm.subscriptions, isEmpty);
+    expect(subscriptions, isEmpty);
 
     final name = 'a random name';
-    realm.subscriptions!.update((mutableSubscriptions) {
+    subscriptions.update((mutableSubscriptions) {
       mutableSubscriptions.addOrUpdate(query, name: name);
     });
 
-    expect(realm.subscriptions!.findByName(name), isNotNull);
+    expect(subscriptions.findByName(name), isNotNull);
 
-    realm.subscriptions!.update((mutableSubscriptions) {
+    subscriptions.update((mutableSubscriptions) {
       mutableSubscriptions.removeByName(name);
     });
 
-    expect(realm.subscriptions, isEmpty);
-    // expect(realm.subscriptions!.findByName(name), isNull);
+    expect(subscriptions, isEmpty);
+    // expect(realm.subscriptions!.findByName(name), isNull); // TODO
 
-    await realm.subscriptions!.waitForStateChange(SubscriptionSetState.complete);
-
-    realm.close();
+    await subscriptions.waitForStateChange(SubscriptionSetState.complete);
   });
 }
