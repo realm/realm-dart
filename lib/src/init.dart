1f4c7ee049c44cb4ba72a3a144508de011e76868
blagoev <lubo@blagoev.com>
2022-06-01 14:32:37 +0300

    get app name on linux

--- lib/src/init.dart
+++ lib/src/init.dart
@@ -75,13 +75,16 @@ DynamicLibrary initRealm() {
     throw Exception("Realm initialization failed. Error: could not initialize Dart APIs");
   }
 
+  print("Checking DIRECTORY NAME"); 
+
   if (isFlutterPlatform && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
     // String pathSeparator = Platform.isWindows ? "\\" : "/";
     String binaryExt = Platform.isWindows ? ".dll" : Platform.isMacOS ? ".dylib" : ".so";
+    String binaryNamePrefix = Platform.isWindows ? "" : "lib";
     // final realmPluginBinaryPath = "${File(realmBinaryPath).absolute.parent}${pathSeparator}realm_plugin$binaryExt";
     // print("realmBinaryPath ${File(realmBinaryPath)}");
     // print("realmPluginBinaryPath: $realmPluginBinaryPath");
-    final realmPluginLib = DynamicLibrary.open("realm_plugin$binaryExt");
+    final realmPluginLib = DynamicLibrary.open("${binaryNamePrefix}realm_plugin$binaryExt");
     final getDirNameFunc = realmPluginLib.lookupFunction<Pointer<Int8> Function(), Pointer<Int8> Function()>("realm_dart_get_app_directory_name");
     final m = getDirNameFunc();
     final dir = m.cast<Utf8>().toDartString();
