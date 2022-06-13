ba7cc13439a4b786ffa191a0f6b3c3283cc26bd3
Desislava Stefanova <dst.stefanova@gmail.com>
2022-06-13 16:26:07 +0300

    Missing import

--- test/subscription_test.dart
+++ test/subscription_test.dart
@@ -26,6 +26,7 @@ import 'package:test/expect.dart';
 import '../lib/realm.dart';
 import '../lib/src/native/realm_core.dart';
 import '../lib/src/subscription.dart';
+import '../lib/src/configuration.dart';
 
 import 'test.dart';
 
@@ -500,7 +501,7 @@ Future<void> main([List<String>? args]) async {
     final realm = getRealm(config);
     expect(() => realm.write(() => realm.add(Task(ObjectId()))), throws<RealmException>("no flexible sync subscription has been created"));
   });
-  
+
   testSubscriptions('Subscription on unqueryable field sould throw', (realm) async {
     realm.subscriptions.update((mutableSubscriptions) {
       mutableSubscriptions.add(realm.all<Event>());
