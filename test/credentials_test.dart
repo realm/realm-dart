6c500bf3b8774b098a004b1bf6d4a6770aad42dc
Desislava Stefanova <dst.stefanova@gmail.com>
2022-08-10 18:38:57 +0300

    Edit token expiration to be 100yars

--- test/credentials_test.dart
+++ test/credentials_test.dart
@@ -300,12 +300,6 @@ Future<void> main([List<String>? args]) async {
     }, throws<RealmException>("failed to reset password for user $username"));
   }, appName: AppNames.autoConfirm);
 
-  /// JWT Header:
-  /// {
-  ///   "kid": "1",
-  ///   "alg": "RS256",
-  ///   "typ": "JWT"
-  /// }
   /// JWT Payload data
   /// {
   ///    "sub": "62f394e9bcb9fee0c9aecb76",  //User.identities.id: If it is a new id the user is created, if it is an existing id the user and profile are updated.
@@ -319,8 +313,8 @@ Future<void> main([List<String>? args]) async {
   ///    "minAge": "10",
   ///    "maxAge": "90",
   ///    "company": "Realm",
-  ///    "iat": 1660130537,
-  ///    "exp": 1660349537, //10 years after Aug 2022
+  ///    "iat": 1660145686,
+  ///    "exp": 4813745686, //100 years after Aug 2022
   ///    "aud": "mongodb.com",
   ///    "iss": "https://realm.io"
   /// }
@@ -330,7 +324,7 @@ Future<void> main([List<String>? args]) async {
     String username = "JWT_privatekey_validated_user@realm.io";
     String userId = "62f394e9bcb9fee0c9aecb76";
     var token =
-        "eyJraWQiOiIxIiwiYWxnIjoiUlMyNTYiLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiI2MmYzOTRlOWJjYjlmZWUwYzlhZWNiNzYiLCJuYW1lIjp7ImZpcnN0TmFtZSI6IkpvaG4iLCJsYXN0TmFtZSI6IkRvZSJ9LCJlbWFpbCI6IkpXVF9wcml2YXRla2V5X3ZhbGlkYXRlZF91c2VyQHJlYWxtLmlvIiwiZ2VuZGVyIjoibWFsZSIsImJpcnRoRGF5IjoiMTk5OS0xMC0xMSIsIm1pbkFnZSI6IjEwIiwibWF4QWdlIjoiOTAiLCJjb21wYW55IjoiUmVhbG0iLCJpYXQiOjE2NjAxMzA1MzcsImV4cCI6MTY2MDM0OTUzNywiYXVkIjoibW9uZ29kYi5jb20iLCJpc3MiOiJodHRwczovL3JlYWxtLmlvIn0.F9GMR41wIQLwYVjgsFOveIFAaqAOj9rVAAPkRxjrvwNPRyvBGRyqMsf7n6mryLWWRmKKiGiS_yH90W5dCoxHP_rxIErD0yAMU-ZxnzTSFeXXu8fEiPb7U08l_VREQp7zn1EolbOvA6W89vjG9RMblCcmnbJvIVJlKTu9kFUkyI6aHTsaT2Ntd5UhJkXh0PAaQe_Sy9Na7dOfmq9mGHoHhmoNPnCyJB-0sy4mrL7vK4Ks935yI0u8IbwnZrYHEKSr9z474HbSSju6YXpuDJ8LGjqXwnkeIQpDqujIeWCn0iTQvWN7dblr0QFGHurCw-UbHJtX6UK9dvbC85TKg8oTLg";
+        "eyJraWQiOiIxIiwiYWxnIjoiUlMyNTYiLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiI2MmYzOTRlOWJjYjlmZWUwYzlhZWNiNzYiLCJuYW1lIjp7ImZpcnN0TmFtZSI6IkpvaG4iLCJsYXN0TmFtZSI6IkRvZSJ9LCJlbWFpbCI6IkpXVF9wcml2YXRla2V5X3ZhbGlkYXRlZF91c2VyQHJlYWxtLmlvIiwiZ2VuZGVyIjoibWFsZSIsImJpcnRoRGF5IjoiMTk5OS0xMC0xMSIsIm1pbkFnZSI6IjEwIiwibWF4QWdlIjoiOTAiLCJjb21wYW55IjoiUmVhbG0iLCJpYXQiOjE2NjAxNDU2ODYsImV4cCI6NDgxMzc0NTY4NiwiYXVkIjoibW9uZ29kYi5jb20iLCJpc3MiOiJodHRwczovL3JlYWxtLmlvIn0.NAy60d4zpzRyJayO9qa6i7T3Yui4vrEJNK5FYhlQGAPCCKmPpBBrPZnOH2QwTsE1sW5jr9EsUPix6PLIauSY4nE-s4JrFb9Yu1QmhzYiXAzzyRK_yJOLmrOujqnWb57Z1KvZo5CsUafTgB5-mbs4t4-udIZEubEgr7sgH51rHK7F1r7EArwT3Fbx-EjPDTN1cWn4945Hku6wk0WgdXwVg6TEaNtT0RrEegw9t63sW1UvOYsgXpHfCePGH8VRX7yYYqu1xBnS1S1ZHNgGNZp3t8pu4lod6jHho0dPetAq9oMSmUP9H2uiKkwqFmWC_bVEjTxX4bGSbLGKZQRkiOn38w";
     final credentials = Credentials.jwt(token);
     final user = await app.logIn(credentials);
 
@@ -348,12 +342,6 @@ Future<void> main([List<String>? args]) async {
     expect(user.profile["company"], "Realm");
   });
 
-  /// JWT Header:
-  /// {
-  ///   "kid": "1",
-  ///   "alg": "RS256",
-  ///   "typ": "JWT"
-  /// }
   /// JWT Payload data
   /// {
   ///    "sub": "62f39187e4b27ad3fb6483ca",  //User.identities.id: If it is a new id the user is created, if it is an existing id the user and profile are updated.
@@ -367,8 +355,8 @@ Future<void> main([List<String>? args]) async {
   ///    "minAge": "10",
   ///    "maxAge": "90",
   ///    "company": "Realm",
-  ///    "iat": 1660130241,
-  ///    "exp": 1660349241, //10 years after Aug 2022
+  ///    "iat": 1660145781,
+  ///    "exp": 4813745781, //100 years after Aug 2022
   ///    "aud": "mongodb.com",
   ///    "iss": "https://realm.io"
   /// }
@@ -378,7 +366,7 @@ Future<void> main([List<String>? args]) async {
     String username = "JWK_URI_validated_user@realm.io";
     String userId = "62f39187e4b27ad3fb6483ca";
     var token =
-        "eyJraWQiOiIxIiwiYWxnIjoiUlMyNTYiLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiI2MmYzOTE4N2U0YjI3YWQzZmI2NDgzY2EiLCJuYW1lIjp7ImZpcnN0TmFtZSI6IkpvaG4iLCJsYXN0TmFtZSI6IkRvZSJ9LCJlbWFpbCI6IkpXS19VUklfdmFsaWRhdGVkX3VzZXJAcmVhbG0uaW8iLCJnZW5kZXIiOiJtYWxlIiwiYmlydGhEYXkiOiIxOTk5LTEwLTExIiwibWluQWdlIjoiMTAiLCJtYXhBZ2UiOiI5MCIsImNvbXBhbnkiOiJSZWFsbSIsImlhdCI6MTY2MDEzMDI0MSwiZXhwIjoxNjYwMzQ5MjQxLCJhdWQiOiJtb25nb2RiLmNvbSIsImlzcyI6Imh0dHBzOi8vcmVhbG0uaW8ifQ.KXpndzjQq2v0K2jLygNU6574VTFQvdyQQH6sEjlDik-ZM7IGVPHZb-o8PWKaMK1DH8V8JEG1fi7Nf5CDHk5XWDgBVcrGfB2qrMq_MZpdPvuSvKbnfN9ZQBME58l9XK-_2RszEPiULy3u_daYCwitNOGFhJ5PJUhC487CNsi3gK947hMMwBDhnF4CcQ68EEE5IRFNcvLbnrDZhKKceYbt0YhGmjTBd-Y0DKF0mO3wUTk-Y4xeXcxd_iVZr-uMa5xbjdsYbI9EwWMmTs6RvW4hWqrAXaAkBbnhrIl_dbGCx6hmCAVtRPNDu7R1H63svITfAgNrzSEDeeIROnU0e9IA4w";
+        "eyJraWQiOiIxIiwiYWxnIjoiUlMyNTYiLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiI2MmYzOTE4N2U0YjI3YWQzZmI2NDgzY2EiLCJuYW1lIjp7ImZpcnN0TmFtZSI6IkpvaG4iLCJsYXN0TmFtZSI6IkRvZSJ9LCJlbWFpbCI6IkpXS19VUklfdmFsaWRhdGVkX3VzZXJAcmVhbG0uaW8iLCJnZW5kZXIiOiJtYWxlIiwiYmlydGhEYXkiOiIxOTk5LTEwLTExIiwibWluQWdlIjoiMTAiLCJtYXhBZ2UiOiI5MCIsImNvbXBhbnkiOiJSZWFsbSIsImlhdCI6MTY2MDE0NTc4MSwiZXhwIjo0ODEzNzQ1NzgxLCJhdWQiOiJtb25nb2RiLmNvbSIsImlzcyI6Imh0dHBzOi8vcmVhbG0uaW8ifQ.Zk2S4WqcPECd2IxPRbGEPkTQ86wxgy_rjSGS5Mlmy8pzVpHE-VRa97qxVZy1zIMNXbVR8jFqbPjSix09f4wclPTqz639kwYr8msnF2DQE2ir7xcw6uOAfwNxVW1YIAkZbETaLiQI2I0Wb7gbEp8CPa1VMbg5D295_jjGNSuAuPgSa352OAL3NrJKHwsmTtRset0x_rcheogW-3fDvi4AQreAqT1xpCgPMq5dvspV6cOVXT0gJ8m1VhIHVZyHngeMkAkdGlIQqVLh1ipDwEuxbpfvt5hyxfXQbUCQs7pmkxCyuSXSr7Kqj3-VEFDtdLFGKuXDG572KlbC4fia9Ap-Dw";
     final credentials = Credentials.jwt(token);
     final user = await app.logIn(credentials);
 
@@ -396,12 +384,6 @@ Future<void> main([List<String>? args]) async {
     expect(user.profile["company"], "Realm");
   }, appName: AppNames.autoConfirm);
 
-  /// JWT Header:
-  /// {
-  ///   "kid": "1",
-  ///   "alg": "RS256",
-  ///   "typ": "JWT"
-  /// }
   /// JWT Payload data
   /// {
   ///   "sub": "62f3840b4ac43f38a50b9e2b", //User.identities.id of the existing user realm-test@realm.io
@@ -409,27 +391,42 @@ Future<void> main([List<String>? args]) async {
   ///     "firstName": "John",
   ///     "lastName": "Doe"
   ///   },
-  ///   "email": "realm-test@realm.io",
+  ///   "email": "jwt_user@#r@D@realm.io",
   ///   "gender": "male",
   ///   "birthDay": "1999-10-11",
   ///   "minAge": "10",
   ///   "maxAge": "90",
   ///   "company": "Realm",
-  ///   "iat": 1660131880,
-  ///   "exp": 1660350880, //10 years after Aug 2022
+  ///   "iat": 1660145055,
+  ///   "exp": 4813745055, //100 years after Aug 2022
   ///   "aud": "mongodb.com",
   ///   "iss": "https://realm.io"
   /// }
   baasTest('JWT - login with existing user and edit profile', (configuration) async {
     final app = App(configuration);
-    final user = await app.logIn(Credentials.emailPassword(testUsername, testPassword));
+    final username = "jwt_user@#r@D@realm.io";
+    final authProvider = EmailPasswordAuthProvider(app);
+    // Always register jwt_user@#r@D@realm.io as a new user.
+    try {
+      await authProvider.registerUser(username, strongPassword);
+    } on RealmException catch (e) {
+      {
+        if (e.message.contains("name already in use")) {
+          // If the user exists, delete it and register a new one with the same name and empty profile
+          final user1 = await loginWithRetry(app, Credentials.emailPassword(username, strongPassword));
+          await app.deleteUser(user1);
+          await authProvider.registerUser(username, strongPassword);
+        }
+      }
+    }
+    final user = await app.logIn(Credentials.emailPassword(username, strongPassword));
     UserIdentity emailIdentity = user.identities.singleWhere((identity) => identity.provider == AuthProviderType.emailPassword);
     expect(emailIdentity.provider, isNotNull);
     var userId = emailIdentity.id;
 
     expect(user.state, UserState.loggedIn);
     expect(user.provider, AuthProviderType.emailPassword);
-    expect(user.profile.email, testUsername);
+    expect(user.profile.email, username);
     expect(user.profile.name, isNull);
     expect(user.profile.gender, isNull);
     expect(user.profile.birthDay, isNull);
@@ -440,7 +437,7 @@ Future<void> main([List<String>? args]) async {
     expect(user.profile["company"], isNull);
 
     var token =
-        "eyJraWQiOiIxIiwiYWxnIjoiUlMyNTYiLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiI2MmYzODQwYjRhYzQzZjM4YTUwYjllMmIiLCJuYW1lIjp7ImZpcnN0TmFtZSI6IkpvaG4iLCJsYXN0TmFtZSI6IkRvZSJ9LCJlbWFpbCI6InJlYWxtLXRlc3RAcmVhbG0uaW8iLCJnZW5kZXIiOiJtYWxlIiwiYmlydGhEYXkiOiIxOTk5LTEwLTExIiwibWluQWdlIjoiMTAiLCJtYXhBZ2UiOiI5MCIsImNvbXBhbnkiOiJSZWFsbSIsImlhdCI6MTY2MDEzMTg4MCwiZXhwIjoxNjYwMzUwODgwLCJhdWQiOiJtb25nb2RiLmNvbSIsImlzcyI6Imh0dHBzOi8vcmVhbG0uaW8ifQ.CXfGI04etO2B98-BA87-1YuQZlcg8Rg62kRjXXxblYaB_I6PDFm9-VfuaBtqtBW8i8DU5gBsoLniByKpy2FhN-CtTImTRu4Zf69jMnKME1l3KMjsYOnXKATSu8RVg9nuSFSAHBzYHOHvSKCKXk_sEH8y4O6GqBGcWHAY2TVlqSCk4ttsg1-lmdCM3fl7P7qwW0bJSeAmTHlm_g-Dh1-4a12_011c9LPoNUKHKwfVsKxn1SJzBzQRDQrn1Zzv_l-rGWCKr7ijrjmDrAi2vDWFxXYhPPFW8Mymn5W0RRV9AmoeUx8gqfyw565l5IHndKEzmsofK6RQtfyFK6EeX-tBQg";
+        "eyJraWQiOiIxIiwiYWxnIjoiUlMyNTYiLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiI2MmYzODQwYjRhYzQzZjM4YTUwYjllMmIiLCJuYW1lIjp7ImZpcnN0TmFtZSI6IkpvaG4iLCJsYXN0TmFtZSI6IkRvZSJ9LCJlbWFpbCI6Imp3dF91c2VyQCNyQERAcmVhbG0uaW8iLCJnZW5kZXIiOiJtYWxlIiwiYmlydGhEYXkiOiIxOTk5LTEwLTExIiwibWluQWdlIjoiMTAiLCJtYXhBZ2UiOiI5MCIsImNvbXBhbnkiOiJSZWFsbSIsImlhdCI6MTY2MDE0NTA1NSwiZXhwIjo0ODEzNzQ1MDU1LCJhdWQiOiJtb25nb2RiLmNvbSIsImlzcyI6Imh0dHBzOi8vcmVhbG0uaW8ifQ.AHi4eh3wT9VifM0Hy07vVa2Sck8qlv4st71GaR5UFaytgDW7a-zhLRpPXYt6RX8mjzx6aCenbVr7-Cg8kKxL8XT5x-kmswse8FVtRXi-G5TU2C3AMuMTavP9KCMSpU6_IUfpF_i8kQbrke-YzfS5jflspyEgxHrHTcG0aRIRqBHAmu78er7t3MMv2tbScmipZv-QOXczhTBt0o2wk8iZ-qqTK2X6xb1wbhUS9YtY4oqmuE7n-I_1xah_yd4yF-aS3n13vT-nrm6aIdjwR_EVxAoekN9TTqs0WzpCjy2CcL-LO3RcepUCPQTGwKg9ObTFjJ2URw4FJ_BEA8EfpT_fBg";
     await user.linkCredentials(Credentials.jwt(token));
 
     UserIdentity jwtIdentity = user.identities.singleWhere((identity) => identity.provider == AuthProviderType.jwt);
@@ -453,8 +450,8 @@ Future<void> main([List<String>? args]) async {
     expect(jwtUser.identities.singleWhere((identity) => identity.provider == AuthProviderType.jwt).id, jwtUserId);
     expect(jwtUser.identities.singleWhere((identity) => identity.provider == AuthProviderType.emailPassword).id, userId);
     expect(jwtUser.provider, AuthProviderType.jwt);
-    expect(jwtUser.profile.email, testUsername);
-    expect(jwtUser.profile.name, testUsername);
+    expect(jwtUser.profile.email, username);
+    expect(jwtUser.profile.name, username);
     expect(jwtUser.profile.gender, "male");
     expect(jwtUser.profile.birthDay, "1999-10-11");
     expect(jwtUser.profile.minAge, "10");
@@ -462,28 +459,22 @@ Future<void> main([List<String>? args]) async {
     expect(jwtUser.profile.firstName, "John");
     expect(jwtUser.profile.lastName, "Doe");
     expect(jwtUser.profile["company"], "Realm");
-  });
+  }, appName: AppNames.autoConfirm);
 
   /// Token signed with private key different than the one configured in Atlas 'flexible' app JWT authentication provider
-  /// JWT Header:
-  /// {
-  ///   "kid": "1",
-  ///   "alg": "RS256",
-  ///   "typ": "JWT"
-  /// }
   /// JWT Payload data
   /// {
-  ///   "sub": "62f396888af8720b373ff06a",
-  ///   "email": "wong_signiture_key@realm.io",
-  ///   "iat": 1660130988,
-  ///   "exp": 1660349988,
-  ///   "aud": "mongodb.com",
-  ///   "iss": "https://realm.io"
+  ///  "sub": "62f396888af8720b373ff06a",
+  ///  "email": "wong_signiture_key@realm.io",
+  ///  "iat": 1660142215,
+  ///  "exp": 4813742215, //100 years after Aug 2022
+  ///  "aud": "mongodb.com",
+  ///  "iss": "https://realm.io"
   /// }
   baasTest('JWT with wrong signiture key - login fails', (configuration) async {
     final app = App(configuration);
     var token =
-        "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MmYzOTY4ODhhZjg3MjBiMzczZmYwNmEiLCJlbWFpbCI6Indvbmdfc2lnbml0dXJlX2tleUByZWFsbS5pbyIsImlhdCI6MTY2MDEzMDk4OCwiZXhwIjoxNjYwMzQ5OTg4LCJhdWQiOiJtb25nb2RiLmNvbSIsImlzcyI6Imh0dHBzOi8vcmVhbG0uaW8ifQ.O133dQ5p2YZBMlGrCW1Pj4LcKq3ud9ILXeRWFc6cO5kgoH-ZJ9QEJ0Xt28Lwkq8btUhhFHa79Xxfapvquk-6zvezRPRIQ7nk1qXhibFJ0_AGusnlEr9TAQIZxWaDDHKzBli3lZz0N5wKkRr-bwi1arSs5Og-KgTjgcJh3g-vfPHjetj9-CFHVBVfqk_wuqpbyhXqlVudaFKmmTmoEWIwYsIqC-2u45RspZsl4GBjU_FRruoxFv5EzdKITxOf5algp02YJd4jCTPmhzjaEOMHNK8LYFlQDZC4KGJGsgDehvhcv86L80IxCpOBHgXZ3Yy5xY7eAyG8mDqtS0yt836cnw";
+        "eyJraWQiOiIxIiwiYWxnIjoiUlMyNTYiLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiI2MmYzOTY4ODhhZjg3MjBiMzczZmYwNmEiLCJlbWFpbCI6Indvbmdfc2lnbml0dXJlX2tleUByZWFsbS5pbyIsImlhdCI6MTY2MDE0MjIxNSwiZXhwIjo0ODEzNzQyMjE1LCJhdWQiOiJtb25nb2RiLmNvbSIsImlzcyI6Imh0dHBzOi8vcmVhbG0uaW8ifQ.Af--ZUCL_KC7lAhrD_d1lq91O7qVwu7GqXifwxKojkLCkbjmAER9K2Xa7BPO8xNstFeX8m9uBo4BCD5B6XmngSmyCj5OZWdiG5LTR_uhA3MnpqcV3Vu40K4Yx8XrjPuCL39xVPnEfPKLGz5TjEcMLa8xMPqo51byX0q3mR2eSS4w1A7c5TiTNuQ23_SCO8aK95SyXwuUmU4mH0iR4sHPtf64WyoAXkx8w5twXExzky1_h473CwtAERdMsBhwz1YzFKP0kxU31pg5SRciF5Ly66sK1fSPTMQPuVdS_wKvAYll8_trWnWS83M3_PWs4UxzOdjSpoK0uqhN-_IC38YOGg";
     final credentials = Credentials.jwt(token);
     expect(() async {
       await app.logIn(credentials);
