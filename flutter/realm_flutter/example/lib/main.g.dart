00c4b0f53502cf1f3bdf17c94aa123f3f467d40f
Kasper Overgård Nielsen <kasper@byolimit.com>
2022-08-12 11:25:04 +0200

    Cannot fully remove build.yaml from flutter/realm_flutter

---
+++ flutter/realm_flutter/example/lib/main.g.dart
@@ -0,0 +1,110 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// RealmObjectGenerator
+// **************************************************************************
+
+class Car extends _Car with RealmEntity, RealmObject {
+  static var _defaultsSet = false;
+
+  Car(
+    String make, {
+    String? model,
+    int? kilometers = 500,
+    Person? owner,
+  }) {
+    if (!_defaultsSet) {
+      _defaultsSet = RealmObject.setDefaults<Car>({
+        'kilometers': 500,
+      });
+    }
+    RealmObject.set(this, 'make', make);
+    RealmObject.set(this, 'model', model);
+    RealmObject.set(this, 'kilometers', kilometers);
+    RealmObject.set(this, 'owner', owner);
+  }
+
+  Car._();
+
+  @override
+  String get make => RealmObject.get<String>(this, 'make') as String;
+  @override
+  set make(String value) => RealmObject.set(this, 'make', value);
+
+  @override
+  String? get model => RealmObject.get<String>(this, 'model') as String?;
+  @override
+  set model(String? value) => RealmObject.set(this, 'model', value);
+
+  @override
+  int? get kilometers => RealmObject.get<int>(this, 'kilometers') as int?;
+  @override
+  set kilometers(int? value) => RealmObject.set(this, 'kilometers', value);
+
+  @override
+  Person? get owner => RealmObject.get<Person>(this, 'owner') as Person?;
+  @override
+  set owner(covariant Person? value) => RealmObject.set(this, 'owner', value);
+
+  @override
+  Stream<RealmObjectChanges<Car>> get changes =>
+      RealmObject.getChanges<Car>(this);
+
+  static SchemaObject get schema => _schema ??= _initSchema();
+  static SchemaObject? _schema;
+  static SchemaObject _initSchema() {
+    RealmObject.registerFactory(Car._);
+    return const SchemaObject(Car, 'Car', [
+      SchemaProperty('make', RealmPropertyType.string),
+      SchemaProperty('model', RealmPropertyType.string, optional: true),
+      SchemaProperty('kilometers', RealmPropertyType.int, optional: true),
+      SchemaProperty('owner', RealmPropertyType.object,
+          optional: true, linkTarget: 'Person'),
+    ]);
+  }
+}
+
+class Person extends _Person with RealmEntity, RealmObject {
+  static var _defaultsSet = false;
+
+  Person(
+    String name, {
+    int age = 1,
+  }) {
+    if (!_defaultsSet) {
+      _defaultsSet = RealmObject.setDefaults<Person>({
+        'age': 1,
+      });
+    }
+    RealmObject.set(this, 'name', name);
+    RealmObject.set(this, 'age', age);
+  }
+
+  Person._();
+
+  @override
+  String get name => RealmObject.get<String>(this, 'name') as String;
+  @override
+  set name(String value) => RealmObject.set(this, 'name', value);
+
+  @override
+  int get age => RealmObject.get<int>(this, 'age') as int;
+  @override
+  set age(int value) => RealmObject.set(this, 'age', value);
+
+  @override
+  Stream<RealmObjectChanges<Person>> get changes =>
+      RealmObject.getChanges<Person>(this);
+
+  static SchemaObject get schema => _schema ??= _initSchema();
+  static SchemaObject? _schema;
+  static SchemaObject _initSchema() {
+    RealmObject.registerFactory(Person._);
+    return const SchemaObject(Person, 'Person', [
+      SchemaProperty('name', RealmPropertyType.string),
+      SchemaProperty('age', RealmPropertyType.int),
+    ]);
+  }
+}
