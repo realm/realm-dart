1b5cfd45283eeb4afbafb0c9aaeac9dc042ad032
Kasper Overgård Nielsen <kasper@byolimit.com>
2022-08-29 11:09:15 +0200

    Add good test for all indexable types

---
+++ generator/test/good_test_data/indexable_types.dart
@@ -0,0 +1,29 @@
+import 'package:realm_common/realm_common.dart';
+
+@RealmModel()
+class _Indexable {
+  @Indexed()
+  late bool aBool;
+  @Indexed()
+  bool? aNullableBool;
+  @Indexed()
+  late int anInt;
+  @Indexed()
+  int? aNullableInt;
+  @Indexed()
+  late String aString;
+  @Indexed()
+  String? aNullableString;
+  @Indexed()
+  late ObjectId anObjectId;
+  @Indexed()
+  ObjectId? aNullableObjectId;
+  @Indexed()
+  late Uuid anUuid;
+  @Indexed()
+  Uuid? aNullableUuid;
+  @Indexed()
+  late DateTime aDateTime;
+  @Indexed()
+  DateTime? aNullableDateTime;
+}
