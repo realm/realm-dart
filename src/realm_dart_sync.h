1dc1dc702788d3cef97200b60ff54cff185c4a0a
blagoev <lubo@blagoev.com>
2022-09-30 22:46:13 +0300

    fix copy of structs through the scheduler. (#924)
    * fix copy of structs through the scheduler.
    
    * add changelog entry and fix changelog
    
    * use the correct struct
    
    * fix changelog link
    
    * remove const from arg, remove unnecessary copies
    
    * add comments
    
    * use std::vector for the temp storage
    
    * use named variables

--- src/realm_dart_sync.h
+++ src/realm_dart_sync.h
@@ -20,7 +20,7 @@
 
 #include <realm.h>
 
-RLM_API void realm_dart_http_request_callback(realm_userdata_t userdata, const realm_http_request_t request, void* request_context);
+RLM_API void realm_dart_http_request_callback(realm_userdata_t userdata, realm_http_request_t request, void* request_context);
 
 RLM_API void realm_dart_sync_client_log_callback(realm_userdata_t userdata, realm_log_level_e level, const char* message);
 
