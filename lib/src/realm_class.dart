7e39023b92b240a97d73ece830202ae6c26f19f4
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-10-11 11:05:50 -0400

    include `DisconnectedSyncConfiguration`
    include so that in reference docs

--- lib/src/realm_class.dart
+++ lib/src/realm_class.dart
@@ -65,6 +65,7 @@ export 'app.dart' show AppConfiguration, MetadataPersistenceMode, App, AppExcept
 export "configuration.dart"
     show
         Configuration,
+        DisconnectedSyncConfiguration,
         FlexibleSyncConfiguration,
         InitialDataCallback,
         InMemoryConfiguration,
