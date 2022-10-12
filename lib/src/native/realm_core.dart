a28eb716876ca06e33074d60d4733fadf9b0786d
Desislava Stefanova <dst.stefanova@gmail.com>
2022-10-12 19:20:33 +0300

    Add cancellation_token to realm_core

--- lib/src/native/realm_core.dart
+++ lib/src/native/realm_core.dart
@@ -24,6 +24,7 @@ import 'dart:io';
 import 'dart:typed_data';
 
 // Hide StringUtf8Pointer.toNativeUtf8 and StringUtf16Pointer since these allows silently allocating memory. Use toUtf8Ptr instead
+import 'package:cancellation_token/cancellation_token.dart';
 import 'package:ffi/ffi.dart' hide StringUtf8Pointer, StringUtf16Pointer;
 import 'package:logging/logging.dart';
 import 'package:path/path.dart' as path;
