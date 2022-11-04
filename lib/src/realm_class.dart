f8ce80f6e952ae0d58ecb8704fb88d15d1212e26
Desislava Stefanova <dst.stefanova@gmail.com>
2022-11-04 12:53:25 +0200

    Reorder  'app.dart' exports

--- lib/src/realm_class.dart
+++ lib/src/realm_class.dart
@@ -57,7 +57,7 @@ export 'package:realm_common/realm_common.dart'
         Uuid;
 
 // always expose with `show` to explicitly control the public API surface
-export 'app.dart' show AppConfiguration, MetadataPersistenceMode, App, AppException;
+export 'app.dart' show AppException, App, MetadataPersistenceMode, AppConfiguration;
 export "configuration.dart"
     show
         AfterResetCallback,
