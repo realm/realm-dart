3e75b992f215a272db22f7977bd4e56b31d1f7e7
Desislava Stefanova <dst.stefanova@gmail.com>
2023-02-01 23:03:56 +0200

    env variables

--- flutter/realm_flutter/tests/integration_test/app_test.dart
+++ flutter/realm_flutter/tests/integration_test/app_test.dart
@@ -1,17 +1,25 @@
 import 'dart:io';
+import 'package:integration_test/integration_test.dart';
+import 'package:test/test.dart';
+
 import 'realm_test.dart' as tests;
 
 Future<void> main() async {
   print("Current PID $pid");
 
-  List<String> testArgs = [
-    getArgFromEnvVariable("BAAS_URL"),
-    getArgFromEnvVariable("BAAS_CLUSTER"),
-    getArgFromEnvVariable("BAAS_API_KEY"),
-    getArgFromEnvVariable("BAAS_PRIVATE_API_KEY"),
-    getArgFromEnvVariable("BAAS_PROJECT_ID"),
-    getArgFromEnvVariable("BAAS_DIFFERENTIATOR"),
-  ];
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  late List<String> testArgs;
+  setUpAll(() {
+    testArgs = [
+      getArgFromEnvVariable("BAAS_URL"),
+      getArgFromEnvVariable("BAAS_CLUSTER"),
+      getArgFromEnvVariable("BAAS_API_KEY"),
+      getArgFromEnvVariable("BAAS_PRIVATE_API_KEY"),
+      getArgFromEnvVariable("BAAS_PROJECT_ID"),
+      getArgFromEnvVariable("BAAS_DIFFERENTIATOR"),
+    ];
+  });
+
   await tests.main(testArgs);
 }
 
