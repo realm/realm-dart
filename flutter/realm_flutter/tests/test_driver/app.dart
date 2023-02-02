50b6a3ce53e22ea7de49d304a185a9ea5a4e3d5b
Desislava Stefanova <dst.stefanova@gmail.com>
2023-02-02 13:00:50 +0200

    returned tests with flutter driver

---
+++ flutter/realm_flutter/tests/test_driver/app.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:flutter_driver/driver_extension.dart';
+import 'realm_test.dart' as tests;
+import 'const.dart';
+
+void main(List<String> args) async {
+  enableFlutterDriverExtension(handler: (command) async {
+    if (command!.startsWith(testCommand)) {
+      //Using the environment since --dart-entrypoint-args= is not working in
+      //Flutter 2.5 with command
+      //flutter drive -t test_driver/app.dart --dart-entrypoint-args="--testname"
+      var testName = const String.fromEnvironment(envTestName);
+      if (testName.isEmpty && args.isNotEmpty) {
+        print("Using testName: \"${args[0]}\" from args");
+        testName = args[0];
+      }
+      List<String> testArgs = [];
+      if (testName.isNotEmpty) {
+        // Build correct test arguments using the dart test arg name '--name'
+        testArgs.addAll(['--name', testName]);
+      }
+
+      if (command.contains("--")) {
+        testArgs.addAll(command.substring(command.indexOf("--")).split(" "));
+      }
+
+      //Invoke the actual Dart tests inside Flutter
+      return await tests.main(testArgs);
+    } else {
+      throw Exception('Unknown command: $command');
+    }
+  });
+}
--- flutter/realm_flutter/tests/test_driver/realm_test.dart
+++ flutter/realm_flutter/tests/test_driver/realm_test.dart
@@ -1,5 +1,11 @@
 import 'dart:async';
 
+import 'package:test/test.dart';
+// ignore: implementation_imports
+import 'package:test_api/src/backend/invoker.dart';
+// ignore: implementation_imports
+import 'package:test_api/src/backend/state.dart' as test_api;
+
 import '../test/app_test.dart' as app_test;
 import '../test/configuration_test.dart' as configuration_test;
 import '../test/credentials_test.dart' as credentials_test;
@@ -16,7 +22,12 @@ import '../test/user_test.dart' as user_test;
 import '../test/client_reset_test.dart' as client_reset_test;
 import '../test/realm_set_test.dart' as realm_set_test;
 
-Future<void> main(List<String> args) async {
+Future<String> main(List<String> args) async {
+  final Completer<String> completer = Completer<String>();
+
+  try {
+    final List<String> failedTests = [];
+
     await app_test.main(args);
     await configuration_test.main(args);
     await credentials_test.main(args);
@@ -32,4 +43,23 @@ Future<void> main(List<String> args) async {
     await user_test.main(args);
     await client_reset_test.main(args);
     await realm_set_test.main(args);
+
+    tearDown(() {
+      if (Invoker.current?.liveTest.state.result == test_api.Result.error || Invoker.current?.liveTest.state.result == test_api.Result.failure) {
+        failedTests.add(Invoker.current!.liveTest.individualName);
+      }
+    });
+
+    tearDownAll(() {
+      if (failedTests.isNotEmpty) {
+        completer.complete(failedTests.join('\n'));
+      } else {
+        completer.complete('');
+      }
+    });
+  } catch (e) {
+    completer.complete(e.toString());
+  }
+
+  return completer.future;
 }
