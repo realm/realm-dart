5fe3acca2d703085e9f2e5f89c947db0709602e7
blagoev <lubo@blagoev.com>
2023-03-23 11:25:23 +0200

    remove old code

--- lib/src/cli/metrics/metrics_command.dart
+++ lib/src/cli/metrics/metrics_command.dart
@@ -138,53 +138,6 @@ Future<void> uploadMetrics(Options options) async {
   }
 }
 
-// Future<Digest> getMachineId() async {
-//   var id = await safe(() async {
-//     if (Platform.isLinux) {
-//       // For linux use /etc/machine-id
-//       // Can be changed by administrator but with unpredictable consequences!
-//       // (see https://man7.org/linux/man-pages/man5/machine-id.5.html)
-//       final process = await Process.start('cat', ['/etc/machine-id']);
-//       return await process.stdout.transform(utf8.decoder).join();
-//     } else if (Platform.isMacOS) {
-//       // For MacOS, use the IOPlatformUUID value from I/O Kit registry in
-//       // IOPlatformExpertDevice class
-//       final process = await Process.start('ioreg', [
-//         '-rd1',
-//         '-c',
-//         'IOPlatformExpertDevice',
-//       ]);
-//       final id = await process.stdout.transform(utf8.decoder).join();
-//       final r = RegExp('"IOPlatformUUID" = "([^"]*)"', dotAll: true);
-//       return r.firstMatch(id)?.group(1); // extract IOPlatformUUID
-//     } else if (Platform.isWindows) {
-//       // For Windows, use the key MachineGuid in registry:
-//       // HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography
-//       // Can be changed by administrator but with unpredictable consequences!
-//       //
-//       // It is generated during OS installation and won't change unless you make
-//       // another OS update or re-install. Depending on the OS version it may
-//       // contain the network adapter MAC address embedded (plus some other numbers,
-//       // including random), or a pseudorandom number.
-//       //
-//       // Consider using System.Identity.UniqueID instead.
-//       // (see https://docs.microsoft.com/en-gb/windows/win32/properties/props-system-identity-uniqueid)
-//       final process = await Process.start(
-//         '${Platform.environment['WINDIR']}\\System32\\Reg.exe',
-//         [
-//           'QUERY',
-//           r'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography',
-//           '/v',
-//           'MachineGuid',
-//         ],
-//       );
-//       return await process.stdout.transform(systemEncoding.decoder).join();
-//     }
-//   }, message: 'failed to get machine id');
-//   id ??= Platform.localHostname; // fallback
-//   return id.strongHash(); // strong hash for privacy
-// }
-
 Future<String> getMachineId() async {
   var id = await safe(() async {
     if (Platform.isLinux) {
