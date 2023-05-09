6c99a27e0d20986299e182fe3e4aa6d161430889
Kasper Overgård Nielsen <kasper@byolimit.com>
2023-05-09 09:32:15 +0200

    Support Decimal128 in mixed + tests

--- common/lib/src/realm_types.dart
+++ common/lib/src/realm_types.dart
@@ -158,7 +158,7 @@ class RealmValue {
   const RealmValue.realmObject(RealmObjectMarker o) : this._(o);
   const RealmValue.dateTime(DateTime timestamp) : this._(timestamp);
   const RealmValue.objectId(ObjectId id) : this._(id);
-  // const RealmValue.decimal128(Decimal128 decimal) : this._(decimal); // not supported yet
+  const RealmValue.decimal128(Decimal128 decimal) : this._(decimal);
   const RealmValue.uuid(Uuid uuid) : this._(uuid);
 
   /// Will throw [ArgumentError]
@@ -172,7 +172,7 @@ class RealmValue {
         o is RealmObjectMarker ||
         o is DateTime ||
         o is ObjectId ||
-        // o is Decimal128 || // not supported yet
+        o is Decimal128 ||
         o is Uuid) {
       return RealmValue._(o);
     } else {
