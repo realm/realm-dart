a69c040db7cf18c603e0d185513e62cb19b84906
Kasper Overgård Nielsen <kasper@byolimit.com>
2023-06-02 16:02:18 +0200

    Split in multiple project and move to melos

---
+++ ejson/packages/ejson/test/person.dart
@@ -0,0 +1,35 @@
+import 'package:ejson/ejson.dart';
+import 'package:ejson_annotation/ejson_annotation.dart';
+
+part 'person.g.dart';
+
+class Person {
+  final String name;
+  final DateTime birthDate;
+  Duration get age => DateTime.now().difference(birthDate);
+
+  final int? cprNumber;
+  final double income;
+  final Person? spouse;
+
+  final children = <Person>[];
+
+  @ejson // annotate constructor to generate decoder and encoder
+  Person(this.name, this.birthDate, this.income, {this.spouse, this.cprNumber});
+
+  @override
+  operator ==(other) =>
+      identical(this, other) ||
+      (other is Person &&
+          other.name == name &&
+          other.birthDate == birthDate &&
+          other.income == income &&
+          other.spouse == spouse);
+
+  @override
+  String toString() =>
+      'Person{name: $name, birthDate: $birthDate, income: $income, spouse: $spouse}';
+
+  @override
+  int get hashCode => Object.hashAll([name, birthDate, income, spouse]);
+}
