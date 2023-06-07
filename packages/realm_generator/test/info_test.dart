174b595999d7d1b512fb6abf469ff83bca059178
Kasper Overgård Nielsen <kasper@byolimit.com>
2023-06-07 16:27:55 +0200

    Rude fix

--- packages/realm_generator/test/info_test.dart
+++ packages/realm_generator/test/info_test.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:test/test.dart';
@@ -8,29 +9,22 @@ void main() async {
   const directory = 'test/info_test_data';
 
   await for (final infoFile in Directory(directory).list(recursive: true).where((f) => f.path.endsWith('.expected')).cast<File>()) {
-    final sb = StringBuffer();
-    var done = false;
-
+    final log = Completer<String>();
     final sourceFile = File(infoFile.path.replaceFirst('.expected', '.dart'));
+
     testCompile(
       'compile $sourceFile',
       sourceFile,
       completes,
-      onLog: (l) {
-        // we want to ignore the logs from other loggers (such as build_resolvers)
-        if (!done && l.loggerName == 'testBuilder') {
-          // disregard all, but first record
-          sb.writeln(l);
-          done = true;
+      onLog: (record) {
+        if (!log.isCompleted && record.loggerName == 'testBuilder') {
+          log.complete('$record'.normalizeLineEndings());
         }
       },
     );
 
     test('log from compile $sourceFile', () {
-      expect(
-        sb.toString().trim(),
-        infoFile.readAsStringSync().trim(),
-      );
+      expect(log.future, completion(infoFile.readAsStringSync()));
     });
   }
 }
