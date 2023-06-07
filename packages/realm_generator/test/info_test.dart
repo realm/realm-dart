4146d700b5815e8c3a29ede746af4771d267b459
Kasper Overgård Nielsen <kasper@byolimit.com>
2023-06-07 18:48:07 +0200

    Force unicode glyphs in error messages
    Instead of reading it from the console. Github windows actions are weird
    in this respect. The logs will alway be rendered with unicode anyway.

--- packages/realm_generator/test/info_test.dart
+++ packages/realm_generator/test/info_test.dart
@@ -1,12 +1,14 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:term_glyph/term_glyph.dart';
 import 'package:test/test.dart';
 
 import 'test_util.dart';
 
 void main() async {
   const directory = 'test/info_test_data';
+  ascii = false; // force unicode glyphs
 
   await for (final infoFile in Directory(directory).list(recursive: true).where((f) => f.path.endsWith('.expected')).cast<File>()) {
     final log = Completer<String>();
@@ -24,7 +26,7 @@ void main() async {
     );
 
     test('log from compile $sourceFile', () {
-      expect(log.future, completion(infoFile.readAsStringSync()));
+      expect(log.future, completion(infoFile.readAsStringSync().normalizeLineEndings()));
     });
   }
 }
