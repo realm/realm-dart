ee862ac1a710cf44a3b2155dd257eb3815d78ae4
Desislava Stefanova <95419820+desistefanova@users.noreply.github.com>
2023-06-15 19:15:27 +0300

    Deprecated `flutter pub run` (#1294)
    Deprecated flutter pub run

--- flutter/realm_flutter/example/README.md
+++ flutter/realm_flutter/example/README.md
@@ -18,3 +18,5 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+##### The "Dart" name and logo and the "Flutter" name and logo are trademarks owned by Google.
--- generator/lib/realm_generator.dart
+++ generator/lib/realm_generator.dart
@@ -19,7 +19,7 @@
 /// Usage
 ///
 /// * Add a dependency to [realm](https://pub.dev/packages/realm) package or [realm_dart](https://pub.dev/packages/realm_dart) package to your application
-/// * Run `flutter pub run build_runner build` or `dart run build_runner build` to generate RealmObjects
+/// * Run `dart run build_runner build` or `dart run build_runner build` to generate RealmObjects
 
 library realm_generator;
 
@@ -33,7 +33,7 @@ export 'src/error.dart';
 /// @nodoc
 Builder generateRealmObjects([BuilderOptions? options]) {
   return SharedPartBuilder(
-      [RealmObjectGenerator()],
-      'realm_objects',
-    );
+    [RealmObjectGenerator()],
+    'realm_objects',
+  );
 }
