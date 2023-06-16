e70bfbb8c83cf3c6dc114827052d712c3541a467
Kasper Overgård Nielsen <kasper@byolimit.com>
2023-06-16 14:12:52 +0200

    2nd attempt

--- ejson/packages/ejson/lib/ejson.dart
+++ ejson/packages/ejson/lib/ejson.dart
@@ -19,5 +19,3 @@ library;
 
 export 'package:ejson_annotation/ejson_annotation.dart';
 export 'package:ejson_core/ejson_core.dart';
-export 'package:ejson_generator/ejson_generator.dart';
-export 'package:ejson_lint/ejson_lint.dart';
