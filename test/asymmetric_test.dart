d5e300ffb98ccea13ea1275969eea9ad352575cc
Kasper Overgård Nielsen <kasper@byolimit.com>
2023-09-15 11:58:00 +0200

    Add asymmetric tests

---
+++ test/asymmetric_test.dart
@@ -0,0 +1,117 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2023 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+import 'package:test/expect.dart' hide throws;
+
+import '../lib/realm.dart';
+import 'test.dart';
+
+part 'asymmetric_test.g.dart';
+
+@RealmModel(ObjectType.asymmetricObject)
+class _Asymmetric {
+  @PrimaryKey()
+  @MapTo('_id')
+  late ObjectId id;
+
+  late List<_Embedded> embeddedObjects;
+}
+
+@RealmModel(ObjectType.embeddedObject)
+class _Embedded {
+  late int value;
+  late RealmValue any;
+  _Symmetric? symmetric;
+}
+
+@RealmModel()
+class _Symmetric {
+  @PrimaryKey()
+  @MapTo('_id')
+  late ObjectId id;
+}
+
+Future<void> main([List<String>? args]) async {
+  await setupTests(args);
+
+  Future<Realm> getSyncRealm(AppConfiguration config) async {
+    return await (AppConfiguration config, List<SchemaObject> schemas) async {
+      final app = App(config);
+      final user = await getAnonymousUser(app);
+      final realmConfig = Configuration.flexibleSync(user, schemas);
+      return getRealm(realmConfig);
+    }(config, [Asymmetric.schema, Embedded.schema, Symmetric.schema]);
+  }
+
+  baasTest('Asymmetric objects die even before upload', (config) async {
+    final realm = await getSyncRealm(config);
+    realm.syncSession.pause();
+
+    final oid = ObjectId();
+    realm.write(() {
+      realm.ingest(Asymmetric(oid, embeddedObjects: [1, 2, 3].map(Embedded.new)));
+    });
+
+    // Find & query on an Asymmetric object is compile time error, but you can cheat with dynamic
+    expect(realm.dynamic.find('Asymmetric', oid), null); // should this throw instead?
+    expect(() => realm.dynamic.all('Asymmetric'), throws<RealmException>('Query on ephemeral objects not allowed'));
+
+    realm.syncSession.resume();
+    await realm.syncSession.waitForUpload();
+  });
+
+  baasTest('Asymmetric re-add same PK', (config) async {
+    final realm = await getSyncRealm(config);
+
+    final oid = ObjectId();
+    realm.write(() {
+      realm.ingest(Asymmetric(oid, embeddedObjects: [1, 2, 3].map(Embedded.new)));
+      expect(() => realm.ingest(Asymmetric(oid, embeddedObjects: [1, 2, 3, 4].map(Embedded.new))),
+          throws<RealmException>("Attempting to create an object of type 'Asymmetric' with an existing primary key value"));
+    });
+
+    realm.write(() {
+      // okay to ingest again in another transaction, because object already dead to us
+      realm.ingest(Asymmetric(oid, embeddedObjects: [1, 2, 3, 5].map(Embedded.new)));
+    });
+
+    await realm.syncSession.waitForUpload();
+  });
+
+  baasTest('Asymmetric tricks to add non-embedded links', (config) async {
+    final realm = await getSyncRealm(config);
+
+    realm.subscriptions.update((mutableSubscriptions) {
+      mutableSubscriptions.add(realm.all<Symmetric>());
+    });
+
+    realm.write(() {
+      final s = realm.add(Symmetric(ObjectId()));
+      // Since this shenanigan is allowed, I have opened a feature request to allow
+      // direct links on a symmetric objects. See https://github.com/realm/realm-core/issues/6976.
+      realm.ingest(Asymmetric(ObjectId(), embeddedObjects: [Embedded(1, symmetric: s)]));
+      realm.ingest(Asymmetric(ObjectId(), embeddedObjects: [Embedded(1, any: RealmValue.from(s))]));
+    });
+
+    await realm.syncSession.waitForUpload();
+  });
+
+  // TODO
+  // Test that asymmetric objects are actually transferred to backend, once we have
+  // a mongoClient to query the backend with.
+}
---
+++ test/asymmetric_test.g.dart
@@ -0,0 +1,139 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'asymmetric_test.dart';
+
+// **************************************************************************
+// RealmObjectGenerator
+// **************************************************************************
+
+class Asymmetric extends _Asymmetric
+    with RealmEntity, RealmObjectBase, AsymmetricObject {
+  Asymmetric(
+    ObjectId id, {
+    Iterable<Embedded> embeddedObjects = const [],
+  }) {
+    RealmObjectBase.set(this, '_id', id);
+    RealmObjectBase.set<RealmList<Embedded>>(
+        this, 'embeddedObjects', RealmList<Embedded>(embeddedObjects));
+  }
+
+  Asymmetric._();
+
+  @override
+  ObjectId get id => RealmObjectBase.get<ObjectId>(this, '_id') as ObjectId;
+  @override
+  set id(ObjectId value) => RealmObjectBase.set(this, '_id', value);
+
+  @override
+  RealmList<Embedded> get embeddedObjects =>
+      RealmObjectBase.get<Embedded>(this, 'embeddedObjects')
+          as RealmList<Embedded>;
+  @override
+  set embeddedObjects(covariant RealmList<Embedded> value) =>
+      throw RealmUnsupportedSetError();
+
+  @override
+  Stream<RealmObjectChanges<Asymmetric>> get changes =>
+      RealmObjectBase.getChanges<Asymmetric>(this);
+
+  @override
+  Asymmetric freeze() => RealmObjectBase.freezeObject<Asymmetric>(this);
+
+  static SchemaObject get schema => _schema ??= _initSchema();
+  static SchemaObject? _schema;
+  static SchemaObject _initSchema() {
+    RealmObjectBase.registerFactory(Asymmetric._);
+    return const SchemaObject(
+        ObjectType.asymmetricObject, Asymmetric, 'Asymmetric', [
+      SchemaProperty('id', RealmPropertyType.objectid,
+          mapTo: '_id', primaryKey: true),
+      SchemaProperty('embeddedObjects', RealmPropertyType.object,
+          linkTarget: 'Embedded', collectionType: RealmCollectionType.list),
+    ]);
+  }
+}
+
+class Embedded extends _Embedded
+    with RealmEntity, RealmObjectBase, EmbeddedObject {
+  Embedded(
+    int value, {
+    RealmValue any = const RealmValue.nullValue(),
+    Symmetric? symmetric,
+  }) {
+    RealmObjectBase.set(this, 'value', value);
+    RealmObjectBase.set(this, 'any', any);
+    RealmObjectBase.set(this, 'symmetric', symmetric);
+  }
+
+  Embedded._();
+
+  @override
+  int get value => RealmObjectBase.get<int>(this, 'value') as int;
+  @override
+  set value(int value) => RealmObjectBase.set(this, 'value', value);
+
+  @override
+  RealmValue get any =>
+      RealmObjectBase.get<RealmValue>(this, 'any') as RealmValue;
+  @override
+  set any(RealmValue value) => RealmObjectBase.set(this, 'any', value);
+
+  @override
+  Symmetric? get symmetric =>
+      RealmObjectBase.get<Symmetric>(this, 'symmetric') as Symmetric?;
+  @override
+  set symmetric(covariant Symmetric? value) =>
+      RealmObjectBase.set(this, 'symmetric', value);
+
+  @override
+  Stream<RealmObjectChanges<Embedded>> get changes =>
+      RealmObjectBase.getChanges<Embedded>(this);
+
+  @override
+  Embedded freeze() => RealmObjectBase.freezeObject<Embedded>(this);
+
+  static SchemaObject get schema => _schema ??= _initSchema();
+  static SchemaObject? _schema;
+  static SchemaObject _initSchema() {
+    RealmObjectBase.registerFactory(Embedded._);
+    return const SchemaObject(ObjectType.embeddedObject, Embedded, 'Embedded', [
+      SchemaProperty('value', RealmPropertyType.int),
+      SchemaProperty('any', RealmPropertyType.mixed, optional: true),
+      SchemaProperty('symmetric', RealmPropertyType.object,
+          optional: true, linkTarget: 'Symmetric'),
+    ]);
+  }
+}
+
+class Symmetric extends _Symmetric
+    with RealmEntity, RealmObjectBase, RealmObject {
+  Symmetric(
+    ObjectId id,
+  ) {
+    RealmObjectBase.set(this, '_id', id);
+  }
+
+  Symmetric._();
+
+  @override
+  ObjectId get id => RealmObjectBase.get<ObjectId>(this, '_id') as ObjectId;
+  @override
+  set id(ObjectId value) => RealmObjectBase.set(this, '_id', value);
+
+  @override
+  Stream<RealmObjectChanges<Symmetric>> get changes =>
+      RealmObjectBase.getChanges<Symmetric>(this);
+
+  @override
+  Symmetric freeze() => RealmObjectBase.freezeObject<Symmetric>(this);
+
+  static SchemaObject get schema => _schema ??= _initSchema();
+  static SchemaObject? _schema;
+  static SchemaObject _initSchema() {
+    RealmObjectBase.registerFactory(Symmetric._);
+    return const SchemaObject(ObjectType.realmObject, Symmetric, 'Symmetric', [
+      SchemaProperty('id', RealmPropertyType.objectid,
+          mapTo: '_id', primaryKey: true),
+    ]);
+  }
+}
