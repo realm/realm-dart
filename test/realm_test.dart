31434f684d9d9db685b457a59ad6199b535127f4
Desislava Stefanova <dst.stefanova@gmail.com>
2023-09-19 08:35:58 +0300

    use default path

--- test/realm_test.dart
+++ test/realm_test.dart
@@ -1535,7 +1535,8 @@ Future<void> main([List<String>? args]) async {
     final app = App(appConfiguration);
     final credentials = Credentials.anonymous(reuseCredentials: false);
     var user = await app.logIn(credentials);
-    final config = Configuration.flexibleSync(user, syncSchema);
+    final path = p.join(Configuration.defaultStoragePath, "${generateRandomString(8)}.realm");
+    final config = Configuration.flexibleSync(user, syncSchema, path: path);
     final beforeCompactSize = await createRealmForCompact(config);
     Future<void>.delayed(Duration(seconds: 5));
 
@@ -1552,7 +1553,8 @@ Future<void> main([List<String>? args]) async {
     final credentials = Credentials.anonymous(reuseCredentials: false);
     var user = await app.logIn(credentials);
     List<int> key = List<int>.generate(encryptionKeySize, (i) => random.nextInt(256));
-    final config = Configuration.flexibleSync(user, syncSchema, encryptionKey: key);
+    final path = p.join(Configuration.defaultStoragePath, "${generateRandomString(8)}.realm");
+    final config = Configuration.flexibleSync(user, syncSchema, encryptionKey: key, path: path);
     final beforeCompactSize = await createRealmForCompact(config);
     Future<void>.delayed(Duration(seconds: 5));
 
