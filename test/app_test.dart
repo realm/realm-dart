58d86179796c2f6b4c0394723318460d7f1f8ef5
Kasper Overgård Nielsen <kasper@byolimit.com>
2024-01-25 22:14:49 +0100

    Fix line-length in app_test.dart

--- test/app_test.dart
+++ test/app_test.dart
@@ -32,19 +32,15 @@ void main() {
   setupTests();
 
   test('AppConfiguration can be initialized', () {
-    Configuration.defaultRealmPath = path.join(
-        Configuration.defaultStoragePath, Configuration.defaultRealmName);
+    Configuration.defaultRealmPath = path.join(Configuration.defaultStoragePath, Configuration.defaultRealmName);
     final defaultAppConfig = AppConfiguration('myapp');
     expect(defaultAppConfig.appId, 'myapp');
-    expect(
-        defaultAppConfig.baseFilePath.path, Configuration.defaultStoragePath);
+    expect(defaultAppConfig.baseFilePath.path, Configuration.defaultStoragePath);
     expect(defaultAppConfig.baseUrl, Uri.parse('https://realm.mongodb.com'));
     expect(defaultAppConfig.defaultRequestTimeout, const Duration(minutes: 1));
-    expect(defaultAppConfig.metadataPersistenceMode,
-        MetadataPersistenceMode.plaintext);
+    expect(defaultAppConfig.metadataPersistenceMode, MetadataPersistenceMode.plaintext);
 
-    final httpClient =
-        HttpClient(context: SecurityContext(withTrustedRoots: false));
+    final httpClient = HttpClient(context: SecurityContext(withTrustedRoots: false));
     final appConfig = AppConfiguration(
       'myapp1',
       baseFilePath: Directory.systemTemp,
@@ -68,8 +64,7 @@ void main() {
     expect(appConfig.appId, 'myapp1');
     expect(appConfig.baseUrl, Uri.parse('https://realm.mongodb.com'));
     expect(appConfig.defaultRequestTimeout, const Duration(minutes: 1));
-    expect(
-        appConfig.metadataPersistenceMode, MetadataPersistenceMode.plaintext);
+    expect(appConfig.metadataPersistenceMode, MetadataPersistenceMode.plaintext);
     expect(appConfig.maxConnectionTimeout, const Duration(minutes: 2));
     expect(appConfig.httpClient, isNotNull);
 
@@ -78,8 +73,7 @@ void main() {
   });
 
   test('AppConfiguration can be created', () {
-    final httpClient =
-        HttpClient(context: SecurityContext(withTrustedRoots: false));
+    final httpClient = HttpClient(context: SecurityContext(withTrustedRoots: false));
     final appConfig = AppConfiguration(
       'myapp1',
       baseFilePath: Directory.systemTemp,
@@ -95,8 +89,7 @@ void main() {
     expect(appConfig.baseFilePath.path, Directory.systemTemp.path);
     expect(appConfig.baseUrl, Uri.parse('https://not_re.al'));
     expect(appConfig.defaultRequestTimeout, const Duration(seconds: 2));
-    expect(
-        appConfig.metadataPersistenceMode, MetadataPersistenceMode.encrypted);
+    expect(appConfig.metadataPersistenceMode, MetadataPersistenceMode.encrypted);
     expect(appConfig.maxConnectionTimeout, const Duration(minutes: 1));
     expect(appConfig.httpClient, httpClient);
 
@@ -128,16 +121,14 @@ void main() {
 
   baasTest('App remove user', (configuration) async {
     final app = App(configuration);
-    final user =
-        await app.logIn(Credentials.emailPassword(testUsername, testPassword));
+    final user = await app.logIn(Credentials.emailPassword(testUsername, testPassword));
 
     expect(user.state, UserState.loggedIn);
     await app.removeUser(user);
     expect(user.state, UserState.removed);
   });
 
-  baasTest('App log out anonymous user is marked as removed',
-      (configuration) async {
+  baasTest('App log out anonymous user is marked as removed', (configuration) async {
     final app = App(configuration);
     final credentials = Credentials.anonymous();
     final user = await app.logIn(credentials);
@@ -173,8 +164,7 @@ void main() {
     final user1 = await app.logIn(Credentials.anonymous());
     expect(app.currentUser, user1);
 
-    final user2 =
-        await app.logIn(Credentials.emailPassword(testUsername, testPassword));
+    final user2 = await app.logIn(Credentials.emailPassword(testUsername, testPassword));
 
     expect(app.currentUser, user2);
 
@@ -188,8 +178,7 @@ void main() {
     expect(app.users.length, 0);
 
     final user = await app.logIn(Credentials.anonymous());
-    final user1 =
-        await app.logIn(Credentials.emailPassword(testUsername, testPassword));
+    final user1 = await app.logIn(Credentials.emailPassword(testUsername, testPassword));
     expect(app.users, [user1, user]);
   });
 
@@ -199,25 +188,20 @@ void main() {
     String username = "realm_tests_do_autoverify${generateRandomEmail()}";
     const String strongPassword = "SWV23R#@T#VFQDV";
     await authProvider.registerUser(username, strongPassword);
-    final user = await loginWithRetry(
-        app, Credentials.emailPassword(username, strongPassword));
+    final user = await loginWithRetry(app, Credentials.emailPassword(username, strongPassword));
     expect(user, isNotNull);
     expect(user.state, UserState.loggedIn);
 
     await app.deleteUser(user);
     expect(user.state, UserState.removed);
 
-    await expectLater(
-        () => loginWithRetry(
-            app, Credentials.emailPassword(username, strongPassword)),
-        throws<AppException>("invalid username/password"));
+    await expectLater(() => loginWithRetry(app, Credentials.emailPassword(username, strongPassword)), throws<AppException>("invalid username/password"));
   });
 
   baasTest('Call Atlas function that does not exist', (configuration) async {
     final app = App(configuration);
     final user = await app.logIn(Credentials.anonymous());
-    await expectLater(user.functions.call('notExisitingFunction'),
-        throws<AppException>("function not found: 'notExisitingFunction'"));
+    await expectLater(user.functions.call('notExisitingFunction'), throws<AppException>("function not found: 'notExisitingFunction'"));
   });
 
   baasTest('Call Atlas function with no arguments', (configuration) async {
@@ -243,8 +227,7 @@ void main() {
     final app = App(configuration);
     final user = await app.logIn(Credentials.anonymous());
     const arg1 = 'Jhonatan';
-    final dynamic response =
-        await user.functions.call('userFuncOneArg', [arg1]);
+    final dynamic response = await user.functions.call('userFuncOneArg', [arg1]);
     expect(response, isNotNull);
     final map = response as Map<String, dynamic>;
     expect(map['arg'], arg1);
@@ -255,41 +238,34 @@ void main() {
     final user = await app.logIn(Credentials.anonymous());
     const arg1 = 'Jhonatan';
     const arg2 = 'Michael';
-    final dynamic response =
-        await user.functions.call('userFuncTwoArgs', [arg1, arg2]);
+    final dynamic response = await user.functions.call('userFuncTwoArgs', [arg1, arg2]);
     expect(response, isNotNull);
     final map = response as Map<String, dynamic>;
     expect(map['arg1'], arg1);
     expect(map['arg2'], arg2);
   });
 
-  baasTest('Call Atlas function with two arguments but pass one',
-      (configuration) async {
+  baasTest('Call Atlas function with two arguments but pass one', (configuration) async {
     final app = App(configuration);
     final user = await app.logIn(Credentials.anonymous());
     const arg1 = 'Jhonatan';
-    final dynamic response =
-        await user.functions.call('userFuncTwoArgs', [arg1]);
+    final dynamic response = await user.functions.call('userFuncTwoArgs', [arg1]);
     expect(response, isNotNull);
     final map = response as Map<String, dynamic>;
     expect(map['arg1'], arg1);
     expect(map['arg2'], <String, dynamic>{'\$undefined': true});
   });
 
-  baasTest('Call Atlas function with two Object arguments',
-      (configuration) async {
+  baasTest('Call Atlas function with two Object arguments', (configuration) async {
     final app = App(configuration);
     final user = await app.logIn(Credentials.anonymous());
     final arg1 = Person("Jhonatan");
     final arg2 = Person('Michael');
-    final dynamic response = await user.functions
-        .call('userFuncTwoArgs', [arg1.toJson(), arg2.toJson()]);
+    final dynamic response = await user.functions.call('userFuncTwoArgs', [arg1.toJson(), arg2.toJson()]);
     expect(response, isNotNull);
     final map = response as Map<String, dynamic>;
-    final receivedPerson1 =
-        PersonExt.fromJson(map['arg1'] as Map<String, dynamic>);
-    final receivedPerson2 =
-        PersonExt.fromJson(map['arg2'] as Map<String, dynamic>);
+    final receivedPerson1 = PersonExt.fromJson(map['arg1'] as Map<String, dynamic>);
+    final receivedPerson2 = PersonExt.fromJson(map['arg2'] as Map<String, dynamic>);
     expect(receivedPerson1.name, arg1.name);
     expect(receivedPerson2.name, arg2.name);
   });
@@ -321,39 +297,21 @@ void main() {
     final app = App(configuration);
     expect(app.currentUser, isNull);
 
-    final user1 =
-        await app.logIn(Credentials.emailPassword(testUsername, testPassword));
+    final user1 = await app.logIn(Credentials.emailPassword(testUsername, testPassword));
     await user1.logOut();
 
     final user2 = await app.logIn(Credentials.anonymous());
     expect(app.currentUser, user2);
     expect(
       () => app.switchUser(user1),
-      throws<RealmException>(
-          "Switch user failed. User is no longer valid or is logged out"),
+      throws<RealmException>("Switch user failed. User is no longer valid or is logged out"),
     );
   });
 
   test('bundleId is salted, hashed and encoded', () {
     final text = isFlutterPlatform ? "realm_tests" : "realm_dart";
-    const salt = [
-      82,
-      101,
-      97,
-      108,
-      109,
-      32,
-      105,
-      115,
-      32,
-      103,
-      114,
-      101,
-      97,
-      116
-    ];
-    final expected =
-        base64Encode(sha256.convert([...salt, ...utf8.encode(text)]).bytes);
+    const salt = [82, 101, 97, 108, 109, 32, 105, 115, 32, 103, 114, 101, 97, 116];
+    final expected = base64Encode(sha256.convert([...salt, ...utf8.encode(text)]).bytes);
     expect(realmCore.getBundleId(), expected);
   });
 
@@ -393,8 +351,7 @@ void main() {
     expect(App.getById(app.id, baseUrl: appConfig.baseUrl), isNotNull);
   });
 
-  baasTest('App(AppConfiguration) on background isolate logs warning',
-      (appConfig) async {
+  baasTest('App(AppConfiguration) on background isolate logs warning', (appConfig) async {
     final receivePort = ReceivePort();
     await Isolate.spawn((args) {
       final logger = Logger.detached('foo');
@@ -417,7 +374,6 @@ void main() {
 }
 
 extension PersonExt on Person {
-  static Person fromJson(Map<String, dynamic> json) =>
-      Person(json['name'] as String);
+  static Person fromJson(Map<String, dynamic> json) => Person(json['name'] as String);
   Map<String, dynamic> toJson() => <String, dynamic>{'name': name};
 }
