95ea148868ae580a0739ac6239485689f0e1bba4
Kasper Overgård Nielsen <kasper@byolimit.com>
2024-02-09 18:47:50 +0100

    Explain mocks

--- packages/realm_generator/test/error_test_data/double_primary_key.realm.dart
+++ packages/realm_generator/test/error_test_data/double_primary_key.realm.dart
@@ -1,2 +1,4 @@
-// MOCK FILE!
+// MOCK FILE! This file exists to ensure the parent file is valid Dart.
+// The parent will be used as input to the realm_generator in a test, and the
+// output compared to the .expected file.
 part of 'double_primary_key.dart';
