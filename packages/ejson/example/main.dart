e8ff3d81e660ca973ff1e722d4d5e0e21359c008
Kasper Overgård Nielsen <kasper@byolimit.com>
2024-02-26 09:15:42 +0100

    Add example to package ejson

--- packages/ejson/example/main.dart
+++ packages/ejson/example/main.dart
@@ -1,1 +1,18 @@
+import 'dart:convert';
 
+import 'package:ejson/ejson.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('round-trip', () {
+    final value = {
+      'abe': [1, 4],
+      'kat': <int>[],
+    };
+    final encoded = toEJson(value);
+    final json = jsonEncode(encoded);
+    print(json);
+    final decoded = fromEJson<Map<String, List<int>>>(jsonDecode(json));
+    expect(value, decoded);
+  });
+}
