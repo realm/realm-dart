523fad870a24ce890511e541157ed73510fca547
Realm CI <robot@realm.io>
2024-02-27 19:32:32 +0100

    [Release 2.0.0-alpha.2] (#1528)
    * [Release 2.0.0-alpha.2]
    
    * Remove revived symlink
    
    ---------
    
    Co-authored-by: nirinchev <nirinchev@users.noreply.github.com>
    Co-authored-by: Nikola Irinchev <irinchev@me.com>

--- packages/realm_dart/lib/src/native/realm_core.dart
+++ packages/realm_dart/lib/src/native/realm_core.dart
@@ -90,7 +90,7 @@ final _pluginLib = () {
 }();
 
 // stamped into the library by the build system (see prepare-release.yml)
-const libraryVersion = '2.0.0-alpha.1';
+const libraryVersion = '2.0.0-alpha.2';
 
 _RealmCore realmCore = _RealmCore();
 
