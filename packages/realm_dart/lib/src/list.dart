3bef5c87a4452384b9c25e1f229ea8a7a9578edd
Kasper Overgård Nielsen <kasper@byolimit.com>
2024-02-28 17:47:47 +0100

    Update copyright

--- packages/realm_dart/lib/src/list.dart
+++ packages/realm_dart/lib/src/list.dart
@@ -1,20 +1,5 @@
-////////////////////////////////////////////////////////////////////////////////
-//
-// Copyright 2021 Realm Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-////////////////////////////////////////////////////////////////////////////////
+// Copyright 2021 MongoDB, Inc.
+// SPDX-License-Identifier: Apache-2.0
 
 import 'dart:core';
 import 'dart:async';
@@ -221,8 +206,7 @@ class UnmanagedRealmList<T extends Object?> extends collection.DelegatingList<T>
 
   UnmanagedRealmList([Iterable<T>? items]) : this._(List<T>.from(items ?? <T>[]));
 
-  UnmanagedRealmList._(super.items)
-      : _base = items;
+  UnmanagedRealmList._(super.items) : _base = items;
 
   @override
   RealmObjectMetadata? get _metadata => throw RealmException("Unmanaged lists don't have metadata associated with them.");
--- packages/realm_dart/test/indexed_test.dart
+++ packages/realm_dart/test/indexed_test.dart
@@ -1,27 +1,11 @@
-////////////////////////////////////////////////////////////////////////////////
-//
-// Copyright 2022 Realm Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-////////////////////////////////////////////////////////////////////////////////
+// Copyright 2022 MongoDB, Inc.
+// SPDX-License-Identifier: Apache-2.0
 
 import 'package:ejson_annotation/ejson_annotation.dart';
 import 'package:ejson/ejson.dart';
 
 import 'dart:math';
 import 'dart:typed_data';
-
 
 import 'package:test/test.dart' hide test, throws;
 import 'test.dart';
