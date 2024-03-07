85128f5fe4d5ca3eb3766209db7c5ea748b948cb
Kasper Overgård Nielsen <kasper@byolimit.com>
2024-03-07 11:27:39 +0100

    Fix good tests

--- packages/realm_generator/test/good_test_data/binary_type.dart
+++ packages/realm_generator/test/good_test_data/binary_type.dart
@@ -7,6 +7,5 @@ part 'binary_type.realm.dart';
 @RealmModel()
 class _Foo {
   late Uint8List requiredBinaryProp;
-  var defaultValueBinaryProp = Uint8List(8);
   late Uint8List? nullableBinaryProp;
 }
