89986542b2ec4951e2bfe776ee2b9e0abcd47393
Nikola Irinchev <irinchev@me.com>
2024-04-09 14:55:29 +0200

    Clean up

--- packages/realm_dart/test/session_test.dart
+++ packages/realm_dart/test/session_test.dart
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import 'dart:async';
-import 'package:realm_dart/src/logging.dart';
 import 'package:test/test.dart' hide test, throws;
 import 'package:realm_dart/realm.dart';
 import 'test.dart';
