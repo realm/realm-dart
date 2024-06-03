967fc1beee8f6a54f390697cfef0a97bf97bba81
Kasper Overgård Nielsen <kasper@byolimit.com>
2024-06-03 10:29:58 +0200

    Avoid dart:ffi dep on Scheduler

--- packages/realm_dart/lib/src/handles/scheduler_handle.dart
+++ packages/realm_dart/lib/src/handles/scheduler_handle.dart
@@ -1,12 +1,14 @@
 // Copyright 2024 MongoDB, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import 'dart:isolate';
+
 import 'handle_base.dart';
 
 import 'native/scheduler_handle.dart' if (dart.library.js_interop) 'web/scheduler_handle.dart' as impl;
 
 abstract interface class SchedulerHandle extends HandleBase {
-  factory SchedulerHandle(int isolateId, int sendPort) = impl.SchedulerHandle;
+  factory SchedulerHandle(int isolateId, SendPort port) = impl.SchedulerHandle;
 
   void invoke(int workQueue);
 }
