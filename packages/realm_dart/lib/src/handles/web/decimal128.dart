1728649f58e3c1c1d001d70c692f0881463f6aa8
Kasper Overgård Nielsen <kasper@byolimit.com>
2024-06-06 15:42:29 +0200

    Implement Decimal128 for web

--- packages/realm_dart/lib/src/handles/web/decimal128.dart
+++ packages/realm_dart/lib/src/handles/web/decimal128.dart
@@ -1,8 +1,11 @@
 // Copyright 2024 MongoDB, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import 'package:decimal/decimal.dart';
+import 'package:realm_dart/src/handles/native/convert.dart';
+import 'package:realm_dart/src/handles/web/web_not_supported.dart';
+
 import '../decimal128.dart' as intf;
-import 'web_not_supported.dart';
 
 class Decimal128 implements intf.Decimal128 {
   static final zero = Decimal128.fromInt(0);
@@ -23,17 +26,57 @@ class Decimal128 implements intf.Decimal128 {
   static final negativeInfinity = -infinity;
 
   /// Parses a string into a [Decimal128]. Returns `null` if the string is not a valid [Decimal128].
-  static Decimal128? tryParse(String source) => webNotSupported();
+  static Decimal128? tryParse(String source) => Decimal.tryParse(source).convert(Decimal128._);
 
   /// Parses a string into a [Decimal128]. Throws a [FormatException] if the string is not a valid [Decimal128].
-  factory Decimal128.parse(String source) => webNotSupported();
+  factory Decimal128.parse(String source) => Decimal128._(Decimal.parse(source));
 
   /// Converts a `int` into a [Decimal128].
-  factory Decimal128.fromInt(int value) => webNotSupported();
+  factory Decimal128.fromInt(int value) => Decimal128._(Decimal.fromInt(value));
 
   /// Converts a `double` into a [Decimal128].
   factory Decimal128.fromDouble(double value) => webNotSupported();
 
+  final Decimal _value;
+  Decimal128._(this._value); // TODO: truncate to 128 bits and handle NaN correctly
+
   @override
-  noSuchMethod(Invocation invocation) => webNotSupported();
+  Decimal128 operator *(covariant Decimal128 other) => Decimal128._(_value * other._value);
+
+  @override
+  Decimal128 operator +(covariant Decimal128 other) => Decimal128._(_value + other._value);
+
+  @override
+  Decimal128 operator -() => Decimal128._(-_value);
+
+  @override
+  Decimal128 operator -(covariant Decimal128 other) => Decimal128._(_value - other._value);
+
+  @override
+  Decimal128 operator /(covariant Decimal128 other) => Decimal128._((_value / other._value).toDecimal());
+
+  @override
+  bool operator <(covariant Decimal128 other) => _value < other._value;
+
+  @override
+  bool operator <=(covariant Decimal128 other) => _value <= other._value;
+
+  @override
+  bool operator >(covariant Decimal128 other) => _value > other._value;
+
+  @override
+  bool operator >=(covariant Decimal128 other) => _value >= other._value;
+
+  @override
+  Decimal128 abs() => Decimal128._(_value.abs());
+
+  @override
+  int compareTo(covariant Decimal128 other) => _value.compareTo(other._value);
+
+  @override
+  // TODO: implement isNaN
+  bool get isNaN => webNotSupported();
+
+  @override
+  int toInt() => _value.toBigInt().toInt();
 }
