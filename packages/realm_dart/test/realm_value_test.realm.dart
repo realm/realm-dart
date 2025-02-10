47732fa6631441bfab233c3f5763bbd0fa544f49
Nikola Irinchev <irinchev@me.com>
2025-02-10 19:48:35 +0100

    Update generated files

--- packages/realm_dart/test/realm_value_test.realm.dart
+++ packages/realm_dart/test/realm_value_test.realm.dart
@@ -6,6 +6,7 @@ part of 'realm_value_test.dart';
 // RealmObjectGenerator
 // **************************************************************************
 
+// coverage:ignore-file
 // ignore_for_file: type=lint
 class TuckedIn extends _TuckedIn
     with RealmEntity, RealmObjectBase, EmbeddedObject {
